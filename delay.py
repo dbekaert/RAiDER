Major update. Fix Zenith calculation. Fix HRRR module.

delay.py
@@ -58,6 +58,9 @@ def _get_lengths(look_vecs):
     '''
     Returns the lengths of a vector or set of vectors
     '''
+    if look_vecs is Zenith:
+        return _ZREF
+
     lengths = np.linalg.norm(look_vecs, axis=-1)
     lengths[~np.isfinite(lengths)] = 0
     return lengths
@@ -124,7 +127,7 @@ def _get_rays(lengths, stepSize, start_positions, scaled_look_vecs):
     positions_l= []
     rayData = zip(lengths, start_positions, scaled_look_vecs)
     for L, S, V in rayData:
-        positions_l.append(_compute_ray(L, S, V))
+        positions_l.append(_compute_ray(L, S, V, stepSize))
 
     return positions_l
 
@@ -154,6 +157,7 @@ def getIntFcn(weatherObj, itype = 'wet', interpType = 'rgi'):
     ifFun = intprn.Interpolator()
     ifFun.setPoints(*weatherObj.getPoints())
     ifFun.setProjection(weatherObj.getProjection())
+
     if itype == 'wet':
         ifFun.getInterpFcns(weatherObj.getWetRefractivity(), interpType = interpType)
     elif itype == 'hydro':
@@ -162,7 +166,7 @@ def getIntFcn(weatherObj, itype = 'wet', interpType = 'rgi'):
  
 
 def _common_delay(weatherObj, lats, lons, heights, 
-                  look_vecs, raytrace, 
+                  look_vecs, 
                   stepSize = _STEP, interpType = 'rgi',
                   verbose = False, nproc = 8):
     """
@@ -190,15 +194,14 @@ def _common_delay(weatherObj, lats, lons, heights,
     """
     import dask.bag as db
     import multiprocessing as mp
-
-    if raytrace:
-        correction = None
-    else:
-        correction = 1/util.cosd(look_vecs)
-        look_vecs = Zenith
-
     if look_vecs is Zenith:
+        _,_,zs = weatherObj.getPoints()
         look_vecs = _getZenithLookVecs(lats, lons, heights, zref = _ZREF)
+        wet_pw  = weatherObj.getWetRefractivity()
+        hydro_pw= weatherObj.getHydroRefractivity()
+        wet_delays = _integrateZenith(zs, wet_pw)
+        hydro_delays = _integrateZenith(zs, hydro_pw)
+        return wet_delays,hydro_delays
 
     if verbose:
         import time
@@ -252,34 +255,50 @@ def _common_delay(weatherObj, lats, lons, heights,
     # give very good  results, in that I'm getting only a factor of 3
     # speed-up for a lot of cores, but that's 1000 seconds faster for 
     # my smal region, so worth doing. 
-    Npart = min(len(newPts)//100 + 1, 400)
-    PntBag = db.from_sequence(newPts, npartitions=Npart)
-    wet_pw = PntBag.map(interpRayWet).compute()
-    hydro_pw = PntBag.map(interpRayHydro).compute()
-    
+
+    useDask = True
+    if useDask:
+        if verbose:
+            print('Beginning interpolation using Dask')
+        Npart = min(len(newPts)//100 + 1, 400)
+        PntBag = db.from_sequence(newPts, npartitions=Npart)
+        wet_pw = PntBag.map(interpRayWet).compute()
+        hydro_pw = PntBag.map(interpRayHydro).compute()
+    else:
+        if verbose:
+            print('Beginning interpolation without Dask')
+        wet_pw, hydro_pw = [], []
+        for pnt in newPts:
+            wet_pw.append(interpRayWet(pnt))
+            hydro_pw.append(interpRayHydro(pnt))
+        
+
     if verbose:
         print('_common_delay: Finished interpolation')
         ft = time.time()
         print('Interpolation took {:4.2f} secs'.format(ft-st))
         print('Average of {:1.6f} secs/ray'.format(.5*(ft-st)/len(newPts)))
         print('_common_delay: finished point-wise delay calculations')
-        print('_common_delay: starting integration')
-        st = time.time()
 
+    delays = _integrateLOS(stepSize, wet_pw, hydro_pw)
+
+    return delays
+
+
+def _integrateLOS(stepSize, wet_pw, hydro_pw):
     delays = [] 
     for d in (wet_pw, hydro_pw):
         delays.append(_integrate_delays(stepSize, d))
-
-    if verbose:
-        print('_common_delay: Finished integration')
-        ft = time.time()
-        print('Integration took {:4.2f} secs'.format(ft-st))
-
-    # Finally apply cosine correction if applicable
-    if correction is not None:
-        delays = [d*correction for d in delays]
-
     return delays
+
+
+def _integrateZenith(zs, pw):
+    return np.trapz(pw, zs, axis = 2)
+
+
+# integrate the delays to get overall delay
+def int_fcn(y, dx):
+    return 1e-6*dx*np.nansum(y)
 
 
 def _integrate_delays(stepSize, refr):
@@ -287,11 +306,6 @@ def _integrate_delays(stepSize, refr):
     This function gets the actual delays by integrating the refractivity in 
     each node. Refractivity is given in the 'refr' variable. 
     '''
-
-    # integrate the delays to get overall delay
-    def int_fcn(y, dx):
-        return 1e-6*dx*np.nansum(y)
-
     delays = []
     for ray in refr:
         delays.append(int_fcn(ray, stepSize))
@@ -305,7 +319,7 @@ def wet_delay(weather, lats, lons, heights, look_vecs, raytrace=True, verbose = 
         print('wet_delay: Running _common_delay for weather.wet_delay')
 
     return _common_delay(weather.wet_delay, lats, lons, heights, look_vecs,
-                         raytrace, verbose)
+                         verbose)
 
 
 def hydrostatic_delay(weather, lats, lons, heights, look_vecs, raytrace=True, verbose = False):
@@ -315,7 +329,7 @@ def hydrostatic_delay(weather, lats, lons, heights, look_vecs, raytrace=True, ve
         print('hydrostatic_delay: Running _common_delay for weather.hydrostatic_delay')
 
     return _common_delay(weather.hydrostatic_delay, lats, lons, heights,
-                         look_vecs, raytrace, verbose)
+                         look_vecs, verbose)
 
 
 def delay_over_area(weather, 
@@ -405,12 +419,13 @@ def delay_from_grid(weather, llas, los, parallel=False, raytrace=True, verbose =
     lats, lons, hts = np.moveaxis(llas, -1, 0)
 
     # Call _common_delay to compute both the hydrostatic and wet delays
-    wet, hydro = _common_delay(weather, lats, lons, hts, los, raytrace = raytrace, verbose = verbose)
+    wet, hydro = _common_delay(weather, lats, lons, hts, los, verbose = verbose)
 #    hydro = hydrostatic_delay(weather, lats, lons, hts, los,
 #    wet = wet_delay(weather, lats, lons, hts, los, raytrace=raytrace, verbose = verbose)
 
     # Restore shape
-    hydro, wet = np.stack((hydro, wet)).reshape((2,) + real_shape)
+    if los is not Zenith:
+        hydro, wet = np.stack((hydro, wet)).reshape((2,) + real_shape)
 
     return hydro, wet
 
@@ -458,12 +473,17 @@ def _tropo_delay_with_values(los, lats, lons, hts,
         los = losreader.infer_los(los, lats, lons, hts, zref)
 
     # We want to test if any shapes are different
-    if (not hts.shape == lats.shape == lons.shape
-            or los is not Zenith and los.shape[:-1] != hts.shape):
+    test1 = hts.shape == lats.shape == lons.shape
+    try:
+        test = los.shape[:-1] != hts.shape
+    except:
+        test2 = los is not Zenith
+
+    if not test1 or test2:
         raise ValueError(
          'I need lats, lons, heights, and los to all be the same shape. ' +
          'lats had shape {}, lons had shape {}, '.format(lats.shape, lons.shape)+
-         'heights had shape {}, and los had shape {}'.format(hts.shape,los.shape))
+         'heights had shape {}, and los was not Zenith'.format(hts.shape))
 
     if verbose: 
         print('_tropo_delay_with_values: called delay_from_grid')
@@ -523,9 +543,9 @@ def tropo_delay(los = None, lat = None, lon = None,
     weather_files = weather['files']
     weather_fmt = weather['name']
 
-    allowedWMTypes = ['ERA-I', 'ERA-5', 'MERRA-2', 'WRF']
-   # if weather_fmt not in allowedWMTypes:
-    raise RuntimeError('Weather model type not allowed/implemented')
+    allowedWMTypes = ['ERA-I', 'ERA-5', 'MERRA-2', 'WRF', 'HRRR', 'pickle']
+    if weather_fmt not in allowedWMTypes:
+        raise RuntimeError('Weather model {} not allowed/implemented'.format(weather_fmt))
 
     # weather model file for storing the weather model
     if wmName is None:
@@ -560,6 +580,7 @@ def tropo_delay(los = None, lat = None, lon = None,
             lats = lat
             lons = lon
             latproj = lonproj = None
+            lon = lat = None
 
     # set_geo_info should be a list of functions to call on the dataset,
     # and each will do some bit of work
@@ -598,7 +619,7 @@ def tropo_delay(los = None, lat = None, lon = None,
         if lats is None:
             lats, lons = wrf.wm_nodes(*weather_files)
     elif weather_type == 'pickle':
-        weather = util.pickle_load('weatherObj.dat')
+        weather = util.pickle_load(weather_files)
     else:
         weather_model = weather_type
         if weather_files is None:
@@ -611,8 +632,9 @@ def tropo_delay(los = None, lat = None, lon = None,
             if not os.path.exists(f):
                 try:
                    weather_model.fetch(lats, lons, time, f)
-                except:
+                except Exception as e:
                    print('ERROR: Unable to download weather data')
+                   print('Exception encountered: {}'.format(e))
                    sys.exit(0)
             else:
                 print('WARNING: Weather model already exists, skipping download')
@@ -624,9 +646,6 @@ def tropo_delay(los = None, lat = None, lon = None,
 
             weather_model.load(f)
             weather = weather_model
-            if verbose:
-                print(weather)
-                #p = weather.plot()
         else:
             weather, xs, ys, proj = weather_model.weather_and_nodes(
                 weather_files)
@@ -639,6 +658,25 @@ def tropo_delay(los = None, lat = None, lon = None,
                 lla = pyproj.Proj(proj='latlong')
                 xgrid, ygrid = np.meshgrid(xs, ys, indexing='ij')
                 lons, lats = pyproj.transform(proj, lla, xgrid, ygrid)
+
+    if weather_type != 'pickle':
+        try:
+            import pickle
+            with open('pickledHRRR.pik', 'wb') as f:
+                pickle.dump(weather, f)
+        except:
+            print('Tried to pickle the weather model, could not')
+
+
+    lons,lats = weather.getLL() 
+    lla = weather.getProjection()
+    if verbose:
+        print(type(weather))
+        print(weather._xs.shape)
+        print(weather)
+        #p = weather.plot(p)
+    import pdb
+    pdb.set_trace()
 
 
     # Height

hrrr.py
@@ -10,6 +10,9 @@ import sys
 import util
 from weatherModel import WeatherModel
 
+def Model():
+    return HRRR()
+
 class HRRR(WeatherModel):
     # I took this from
     # https://www.ecmwf.int/en/forecasts/documentation-and-support/137-model-levels.
@@ -68,9 +71,22 @@ class HRRR(WeatherModel):
         if download_only:
             self._download_pressure_levels(dateTime, filename, nProc = nProc)
         elif filename is not None:
-            self._load_pressure_levels(filename = filename)
+            #TODO: either implement a NETCDF version of this, or handle both HDF5 and NETCDF
+            f = filename.replace('.nc', '.hdf5')
+            try:
+                self._load_pressure_levels(filename = f)
+            except: 
+                self._download_pressure_levels(dateTime, f, nProc = nProc)
+                self._load_pressure_levels(filename = f)
         else:
             self._load_pressure_levels(dateTime, nProc = nProc)
+
+        self._find_e()
+        self._get_wet_refractivity()
+        self._get_hydro_refractivity() 
+        
+        # adjust the grid based on the height data
+        self._adjust_grid()
 
 
     def _restrict_model(self, lat_min, lat_max, lon_min, lon_max):
@@ -79,19 +95,68 @@ class HRRR(WeatherModel):
         points left in the model (because e.g. the ROI is outside the US), raise
         an exception. 
         '''
-        mask = (self._xs > lon_min) & (self._xs < lon_max) & (self._ys > lat_min) & (self._ys < lat_max)
 
-        # if there are on points left, raise exception
-        if np.sum(mask) == 0:
+        # Have to do some complicated stuff to get a nice square box without doing the whole US
+        self._xs = self._xs.swapaxes(0,1)
+        self._ys = self._ys.swapaxes(0,1)
+        self._zs = self._zs.swapaxes(0,1)
+        self._rh = self._rh.swapaxes(0,1)
+        self._p = self._t.swapaxes(0,1)
+        self._t = self._t.swapaxes(0,1)
+
+        mask1 = (self._xs[...,0] > lon_min) & (self._xs[...,0] < lon_max)
+        mask3 = (self._ys[...,0] > lat_min) & (self._ys[...,0] < lat_max)
+        mask2 = np.sum(mask1, axis = 0).astype('bool')
+        mask4 = np.sum(mask3, axis = 1).astype('bool')
+        NptsX = self._xs.shape[1]
+        NptsY = self._xs.shape[0]
+        lonRange = np.arange(0,NptsX)
+        latRange = np.arange(0,NptsY)
+        lonx = lonRange[mask2]
+        latx = latRange[mask4]
+        zx = np.arange(0, self._zs.shape[2])
+
+        # if there are no points left, raise exception
+        if np.sum(mask2) == 0 or np.sum(mask4)==0:
             raise RuntimeError('Region of interest is outside the region of the HRRR weather archive data')
 
         # otherwise subset the data
-        self._xs = self._xs[mask]
-        self._ys = self._ys[mask]
-        self._zs = self._zs[mask]
-        self._rh = self._rh[mask]
-        self._t  = self._t[mask]
-        self._p  = self._p[mask]
+        _xs = self._xs[np.ix_(latx,lonx, zx)]
+        _ys = self._ys[np.ix_(latx,lonx, zx)]
+        _zs = self._zs[np.ix_(latx,lonx, zx)]
+        _rh = self._rh[np.ix_(latx,lonx, zx)]
+        _t  = self._t[np.ix_(latx,lonx, zx)]
+        _p  = self._p[np.ix_(latx,lonx, zx)]
+
+
+#TODO: Will need to somehow handle the non-uniform grid for interpolation
+#        # Finally interpolate to a regular grid (need to avoid this in future)
+#        xbounds = (np.min(_xs), np.max(_xs))
+#        ybounds = (np.min(_ys), np.max(_ys))
+#        xstep = ystep = 0.03 # ~3 km grid spacing in degrees
+#        _xsN, _ysN = getNewXY(xbounds, ybounds, xstep, ystep)
+#        self._zs = interp2DLayers(_xs, _ys, _zs,_xsN, _ysN)
+#        self._rh = interp2DLayers(_xs, _ys, _rh,_xsN, _ysN)
+#        self._t  = interp2DLayers(_xs, _ys, _t, _xsN, _ysN)
+#        self._p  = interp2DLayers(_xs, _ys, _p, _xsN, _ysN)
+      
+        #self._wet_refractivity = interp2DLayers(_xs, _ys, _wrf, _xsN, _ysN)
+        #self._hydrostatic_refractivity = interp2DLayers(_xs, _ys, _hrf, _xsN, _ysN)
+#        self._xs = np.broadcast_to(_xs[..., np.newaxis],
+#                                     self._zs.shape)
+#        self._ys = np.broadcast_to(_ys[..., np.newaxis],
+#                                     self._zs.shape)
+
+        self._zs = _zs.copy()
+        self._ys = _ys.copy()
+        self._xs = _xs.copy()
+        self._rh = _rh.copy()
+        self._t  = _t.copy()
+        self._p  = _p.copy()
+
+        self._find_e_from_rh()
+        self._get_wet_refractivity()
+        self._get_hydro_refractivity() 
 
 
     def _download_pressure_levels(self, dateTime, filename, nProc = 16):
@@ -110,7 +175,7 @@ class HRRR(WeatherModel):
 
         # download the 'raw' data
         lats, lons, temps, rhs, z, pl = makeDataCubes(dateTime, nProc = nProc)
-        Npl = len(pl['Values'])
+        Npl = len(pl)
         Ny, Nx = lats.shape
 
         # save the data to an HDF5 file
@@ -120,7 +185,7 @@ class HRRR(WeatherModel):
 
             pld = f.create_dataset('Pressure_levels', (Npl, ), 'f')
             pld.attrs['help'] = 'Pressure levels'
-            pld[:] = np.array(pl['Values'])
+            pld[:] = np.array(pl)
 
             lat = f.create_dataset('lats', (Ny, Nx), 'f')
             lat.attrs['help'] = 'Latitude'
@@ -153,21 +218,14 @@ class HRRR(WeatherModel):
               doi: 10.1016/j.cageo.2017.08.005.
         '''
         if filename is not None:
-            lats, lons, temps, rhs, z, pl = self._load_pressure_levels_from_file(filename)
+            lats, lons, temps, rhs, geo_hgt, pl = self._load_pressure_levels_from_file(filename)
         else:
-            lats, lons, temps, rhs, z, pl = makeDataCubes(dateTime, nProc = nProc)
-
-        lons = lons.T
-        lats = lats.T
-        rhs  = np.swapaxes(rhs, 0, 1)
-        z    = np.swapaxes(z, 0, 1)
-        temps= np.swapaxes(temps, 0, 1)
+            lats, lons, temps, rhs, geo_hgt, pl = makeDataCubes(dateTime, nProc = nProc)
 
         lons[lons > 180] -= 360
         self._proj = pyproj.Proj(proj='latlong')
 
         # data cube format should be lons, lats, heights
-        geo_hgt = z/self._g0
         _lons = np.broadcast_to(lons[..., np.newaxis],
                                      geo_hgt.shape)
         _lats = np.broadcast_to(lats[..., np.newaxis],
@@ -184,6 +242,12 @@ class HRRR(WeatherModel):
 
         self._xs = _lons
         self._ys = _lats
+
+        # flip stuff around to match convention
+        self._p = np.flip(self._p, axis = 2)
+        self._t = np.flip(self._t, axis = 2)
+        self._zs = np.flip(self._zs, axis = 2)
+        self._rh = np.flip(self._rh, axis = 2)
 
 
     def _load_pressure_levels_from_file(self, fileName):
@@ -215,6 +279,8 @@ def makeDataCubes(dateTime = None, nProc = 16):
     rhList = getRH(dateTime, pl['Values'], nProc= nProc)
     zList = getZ(dateTime, pl['Values'], nProc= nProc)
 
+    pl = [convertmb2Pa(p) for p in pl['Values']]
+
     try:
        temps = stackList(tempList)
     except:
@@ -225,8 +291,11 @@ def makeDataCubes(dateTime = None, nProc = 16):
 
     lats, lons = getLatLonsFromList(zList)
 
-    return lats, lons, temps, rhs, zs, pl
+    return lats.T, lons.T, temps.swapaxes(0,1), rhs.swapaxes(0,1), zs.swapaxes(0,1), pl
 
+
+def convertmb2Pa(pres):
+    return 100*pres
 
 def getLatLonsFromList(List):
     return List[0]['lat'], List[0]['lon']
@@ -310,7 +379,7 @@ def get_hrrr_variable(DATE, variable,
                       field='sfc',
                       removeFile=True,
                       value_only=False,
-                      verbose=False,
+                      verbose=True,
                       outDIR='./'):
     """
     Uses cURL to grab the requested variable from a HRRR grib2 file in the
@@ -602,3 +671,35 @@ def get_hrrr_variable(DATE, variable,
                 'URL': grib2file}
 
 
+def interp2DLayers(xs, ys, values, xnew, ynew):
+    '''
+    Implement a 2D interpolator to transform the non-uniform
+    HRRR grid to a uniform lat-long grid. This should be updated
+    in future to be avoided. 
+    '''
+    from scipy.interpolate import griddata
+
+    newLayer = []
+    for layerNum, layer in enumerate(np.moveaxis(values, 2, 0)):
+        newVals = griddata((xs[..., layerNum].flatten(), ys[..., layerNum].flatten()), 
+                     layer.flatten(), 
+                     (xnew, ynew), 
+                     method = 'linear')
+        newLayer.append(newVals)
+
+    newVals = np.stack(newLayer, axis =2)
+
+    return newVals
+
+
+def getNewXY(xbounds, ybounds, xstep, ystep):
+    '''
+    Get new uniform X,Y values from the bounds
+    '''
+    xnew = np.arange(xbounds[0], xbounds[1], xstep)
+    ynew =  np.arange(ybounds[0], ybounds[1], ystep)
+    [X, Y] = np.meshgrid(xnew, ynew)
+    
+    return X, Y
+
+    

interpolator.py
@@ -45,6 +45,21 @@ class Interpolator:
         string += 'Current Projection: {}\n'.format(self._proj.srs if self._proj.srs is not None else None)
         string += 'Number of functions to interpolate: {}\n'.format(len(self._interp))
         string += '='*30 + '\n'
+
+        s2 = '\n'
+        s2 += 'Lat/Lon bounding box: \n'
+        s2 += '\n'
+        s2 += '-------{:.2f}-------\n'.format(self._bbox[1][1])
+        s2 += '|                   |\n'
+        s2 += '|                   |\n'
+        s2 += '|                   |\n'
+        s2 += '{:.2f}         {:.2f}\n'.format(self._bbox[0][0], self._bbox[1][0])
+        s2 += '|                   |\n'
+        s2 += '|                   |\n'
+        s2 += '|                   |\n'
+        s2 += '-------{:.2f}-------\n'.format(self._bbox[0][1])
+        string += s2
+
         return str(string)
 
     def _getShape(self, xs, ys, zs):
@@ -202,7 +217,9 @@ def _interp3D(xs, ys, zs, values, shape):
     # the native weather grid projection
     xvalues = np.unique(xs)
     yvalues = np.unique(ys)
-    interp= rgi((xvalues, yvalues, zvalues), new_var,
+
+    # TODO: temporarily switch x, y values until can confirm
+    interp= rgi((yvalues,xvalues, zvalues), new_var,
                            bounds_error=False, fill_value = np.nan)
     return interp
 
@@ -404,8 +421,10 @@ def interp_along_axis(oldCoord, newCoord, data, axis = 2, inverse=False, method=
         print('Values outside the valid range will be filled in '\
               'with NaNs or the closest non-zero value (default)')
     if np.any(np.diff(_x, axis=0) < 0):
+        print(_x[:,0,0])
         raise ValueError('x should increase monotonically')
     if np.any(np.diff(_newx, axis=0) < 0):
+        print(newx)
         raise ValueError('newx should increase monotonically')
 
     # Cubic interpolation needs the gradient of y in addition to its values

weatherModel.py
@@ -67,7 +67,7 @@ class WeatherModel():
     def __repr__(self):
         string = '\n'
         string += '======Weather Model class object=====\n'
-        string += 'Number of points in Lat/Lon = {}/{}\n'.format(*self._xs.shape[:2])
+        string += 'Number of points in Lon/Lat = {}/{}\n'.format(*self._xs.shape[:2])
         string += 'Total number of grid points (3D): {}\n'.format(np.prod(self._xs.shape))
         string += 'Latitude resolution: {}\n'.format(self._lat_res)
         string += 'Longitude resolution: {}\n'.format(self._lon_res)
@@ -92,6 +92,13 @@ class WeatherModel():
         string += 'A: {}\n'.format(self._a)
         string += 'B: {}\n'.format(self._b)
         return str(string)
+
+    def plot(self, index):
+        import matplotlib.pyplot as plt
+        plt.plot(self._p[index, index, :], self._zs[index, index, :])
+        plt.xlabel('Pressure')
+        plt.ylabel('Z')
+        plt.show()
 
     def Model(self):
         return self._Name
@@ -382,7 +389,10 @@ class WeatherModel():
         return self._proj
 
     def getPoints(self):
-        return self._xs,self._ys, self._zs
+        return self._xs.copy(),self._ys.copy(), self._zs.copy()
+        
+    def getLL(self):
+        return self._ys[...,0].copy(), self._xs[...,0].copy()
         
 
 