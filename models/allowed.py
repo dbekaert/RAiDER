b1c5fb0e321ef4575e72bbfb8858ead4633077c5
Jeremy Maurer <maurer.jeremy@gmail.com>
2019-08-05 12:07:06 -0700

    add helper code for allowed models

---
+++ models/allowed.py
@@ -0,0 +1,26 @@
+
+def AllowedModels():
+    '''
+    return a list of the implemented model types
+    '''
+    allowedModels = [
+      'ERA-5',
+      'ERA-I',
+      'MERRA-2',
+      'WRF',
+      'HRRR',
+      'pickle',
+      'grib']
+
+    return allowedModels
+
+
+def checkModel(modelName):
+    '''
+    Check whether the input model name has been implemented
+    '''
+    allowedWMTypes = AllowedModels()
+    if modelName not in allowedWMTypes:
+        raise RuntimeError('Weather model {} not allowed/implemented'.format(weather_fmt))
+
+
