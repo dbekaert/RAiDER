f9ab41a006bdfbf3cd98020b36db972113db0bb4
Jeremy Maurer <maurer.jeremy@gmail.com>
2019-08-12 12:52:54 -0700

    Try a new setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from distutils.core import setup, Extension
-import numpy
+import numpy as np
 from Cython.Build import cythonize
 import os
 
-# Paths to code requiring compilation
-print('Installing RAiDER')
+
+def getVersion():
+    '''
+    Load the version from a text file
+    '''
+    with open('version.txt', 'r') as f:
+        return f.read().split('=')[-1].replace('\'', '').strip()
+    except IOError:
+        return "0.0.0a1"
+
 
 ## geometry extension
 obj_files = ['geometry']
@@ -34,24 +42,46 @@ os.environ["CXX"] = 'g++'
 
 """
 extensions = [Extension(name="RAiDER.demo", sources=geometry_source_files,
-                        include_dirs=[numpy.get_include()] + cls_dirs,
+                        include_dirs=[np.get_include()] + cls_dirs,
                         extra_compile_args=['-std=c++11'],
                         extra_link_args=['-lm'],
                         library_dirs=[geometry_lib_dir],
                         libraries=['geometry'],
                         language="c++")]
-"""
     
-extensions = [Extension(name="RAiDER.demo", sources=geometry_source_files,
-                        include_dirs=[numpy.get_include()] + cls_dirs,
+extensions = [Extension(name="RAiDER.extension", sources=geometry_source_files,
+                        include_dirs=[np.get_include()] + cls_dirs,
                         language="c++")]
+"""
+extensions = [Extension(name="RAiDER.extension", sources=geometry_source_files,
+                        include_dirs=[np.get_include()] + cls_dirs]
 
 setup (name = 'RAiDER',
        version = '1.0',
        description = 'This is the RAiDER package',
+       cmdclass={'build_ext': Cython.Build.build_ext},
        ext_modules = cythonize(extensions, nthreads=8),
-       packages=['RAiDER'],
-       package_dir={'RAiDER': 'tools/RAiDER','RAiDER.models': 'tools/RAiDER/models'},
+       zip_safe=False,
+#       setup_requires=['numpy>=1.0', 'cython>=0.24.1'],
+#       install_requires=['numpy>=1.0', 'nose>=0.11', 'cython>=0.24.1'],
+       packages=['RAiDER', 'RAiDER.models', 'RAiDER.geometry'],
+       package_dir={'RAiDER': 'tools/RAiDER','RAiDER.models': 'tools/RAiDER/models', 'RAiDER.geometry': 'tools/bindings/geometry/'},
        scripts=['tools/bin/raiderDelay.py'])
 
+'''
+
+setup(
+    package_dir={'pycydemo': 'pycydemo'},
+    packages=['pycydemo', 'pycydemo.tests'],
+    ext_modules=[Extension(
+        'pycydemo.extension',
+        sources=['pycydemo/extension.pyx'],
+        include_dirs=[np.get_include()],
+    )],
+    zip_safe=False,
+    setup_requires=['numpy>=1.0', 'cython>=0.24.1'],
+    install_requires=['numpy>=1.0', 'nose>=0.11', 'cython>=0.24.1'],
+)
+
+'''
 
