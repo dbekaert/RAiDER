03f6e0916aca02962236951622d7e8340e4f4841
Jeremy Maurer <maurer.jeremy@gmail.com>
2019-08-15 14:09:11 -0700

    pull the updated setup.py file from the chunkedInterp branch

--- setup.py
+++ setup.py
@@ -6,6 +6,21 @@
 # RESERVED. United States Government Sponsorship acknowledged.
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+A note about setup and installation of this module:
+This package uses GDAL and g++, both of which can be tricky to set up correctly.
+GDAL in particular will often break after installing a new program
+If you receive error messages such as the following: 
+ImportError: ~/anaconda3/envs/RAiDER/lib/python3.7/site-packages/matplotlib/../../../libstdc++.so.6: version `CXXABI_1.3.9' not found (required by ~/anaconda3/envs/RAiDER/lib/python3.7/site-packages/matplotlib/ft2font.cpython-37m-x86_64-linux-gnu.so)
+or
+ImportError: libtiledb.so.1.6.0: cannot open shared object file: No such file or directory
+or
+***cmake: /u/kriek1/maurer/software/anaconda3/envs/RAiDER/bin/../lib/libstdc++.so.6: version `GLIBCXX_3.4.20' not found (required by cmake)***
+
+try running the following commands within your RAiDER conda environment:
+conda update --force-install libstdcxx-ng
+conda update --force-install gdal libgdal
+'''
 import numpy as np
 import os
 import subprocess as subp
@@ -38,7 +53,11 @@ def getVersion():
           return "0.0.0a1"
 
 
-def srcFiles(GEOMETRY_DIR, GEOMETRY_LIB_DIR):
+def delayFiles(DELAY_DIR):
+   src_files = [os.path.join(DELAY_DIR, 'get_rays.pyx')]
+   return src_files
+
+def geomFiles(GEOMETRY_DIR):
    # geometry extension
    obj_files = ['Geometry']
 
@@ -53,6 +72,7 @@ def clsDirs(GEOMETRY_DIR):
    # geometry classes
    cls_dirs = [os.path.join(GEOMETRY_DIR, "cpp/classes/Geometry"), 
                os.path.join(GEOMETRY_DIR, "cpp/classes/Orbit"),
+               GEOMETRY_LIB_DIR,
                os.path.join(GEOMETRY_DIR, "cpp/classes/Utility")]
 
    return cls_dirs
@@ -85,13 +105,21 @@ def mkdir(dirName):
 extensions = [
      Extension(
        name="Geo2rdr",
-       sources=srcFiles(GEOMETRY_DIR, GEOMETRY_LIB_DIR),
+       sources=geomFiles(GEOMETRY_DIR),
        include_dirs=[np.get_include()] + clsDirs(GEOMETRY_DIR), 
        extra_compile_args=['-std=c++11'],
        extra_link_args=['-lm'],
        library_dirs=[GEOMETRY_LIB_DIR],
        libraries=['geometry'],
-       language="c++")
+       language="c++"
+     )
+     Extension(
+       name="get_rays", 
+       include_dirs=[np.get_include()], 
+       sources=(delayFiles(DELAY_DIR)),
+       extra_compile_args=['-std=c++11'],
+       language="c++"
+     )
 ]
 
 makeCPP(GEOMETRY_DIR)
@@ -101,8 +129,9 @@ setup (name = 'RAiDER',
        description = 'This is the RAiDER package',
        package_dir={'tools': 'tools',
                     'RAiDER': 'tools/RAiDER',
+                    'geometry': 'build',
                     'RAiDER.models': 'tools/RAiDER/models'},
-       packages=['tools', 'RAiDER', 'RAiDER.models'],
+       packages=['tools', 'RAiDER', 'RAiDER.models', 'geometry'],
        #packages=[''],
        ext_modules = cythonize(extensions, quiet = True,nthreads=NTHREADS),
        scripts=['tools/bin/raiderDelay.py'])
