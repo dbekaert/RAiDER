0e18c4eda3550a1a94a332fa93b607bff45c9a4e
Jeremy Maurer <maurer.jeremy@gmail.com>
2019-09-09 12:14:29 -0700

    Change default ZREF to 25 km; seems to have maximal impact on resutls for minimal impact on integration time'

--- tools/RAiDER/constants.py
+++ tools/RAiDER/constants.py
@@ -8,7 +8,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 _ZMIN = -100   # minimum required height
-_ZREF = 15000  # maximum requierd height
+_ZREF = 25000  # maximum requierd height
 _STEP = 15     # integration step size in meters
 
 _g0 = 9.80665
