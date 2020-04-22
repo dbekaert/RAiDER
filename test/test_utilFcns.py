d03e98b87df7950a82fd3f0169581e1f47119d6d
Yang Lei <leiyangfrancis@gmail.com>
2020-04-22 13:55:33 -0700

    Add files via upload

--- test/test_utilFcns.py
+++ test/test_utilFcns.py
@@ -10,14 +10,62 @@ import unittest
 
 class FcnTests(unittest.TestCase):
 
+    # test sind
     theta1 = np.array([0,30,90,180])
     truev1 = np.array([0,0.5,1,0])
+    
+    # test cosd
+    theta2 = np.array([0,60,90,180])
+    truev2 = np.array([1,0.5,0,-1])
+    
+    # test tand
+    theta3 = np.array([0,30,45,60])
+    truev3 = np.array([0,1/np.sqrt(3),1,np.sqrt(3)])
+    
+    # test gdal_open
+    fname1 = '../test/test_geom/lat.rdr'
+    shape1 = (226,45)
+    
+    # test writeResultsToHDF5
+    lats = np.array([15.0, 15.5, 16.0, 16.5, 17.5, -40, 60, 90])
+    lons = np.array([-100.0, -100.4, -91.2, 45.0, 0., -100,-100, -100])
+    hgts = np.array([0., 1000., 10000., 0., 0., 0., 0., 0.])
+    wet = np.zeros(lats.shape)
+    hydro = np.ones(lats.shape)
+    filename = 'dummy.hdf5'
 
     # test error messaging
     def test_sind(self):
         from RAiDER.utilFcns import sind
         out =sind(self.theta1)
         self.assertTrue(np.allclose(out, self.truev1))
+
+    def test_cosd(self):
+        from RAiDER.utilFcns import cosd
+        out =cosd(self.theta2)
+        self.assertTrue(np.allclose(out, self.truev2))
+
+    def test_tand(self):
+        from RAiDER.utilFcns import tand
+        out =tand(self.theta3)
+        self.assertTrue(np.allclose(out, self.truev3))
+
+    def test_gdal_open(self):
+        from RAiDER.utilFcns import gdal_open
+        out = gdal_open(self.fname1, False)
+        self.assertTrue(np.allclose(out.shape, self.shape1))
+
+    def test_writeResultsToHDF5(self):
+        from RAiDER.utilFcns import writeResultsToHDF5
+        writeResultsToHDF5(self.lats, self.lons, self.hgts, self.wet, self.hydro, self.filename)
+        import h5py
+        with h5py.File(filename, 'r') as f:
+            lats = f['lat']
+            hydro = f['hydroDelay']
+            delayType = f.attrs['DelayType']
+        self.assertTrue(np.allclose(lats, self.lats))
+        self.assertTrue(np.allclose(hydro, self.hydro))
+        self.assertEqual(delayType, 'Zenith')
 
   
 
