f3a29a59068181de780cbcff847343396633b266
Yang Lei <leiyangfrancis@gmail.com>
2020-06-09 10:53:59 -0700

    updated delayFcns.py to use hdf5 parallel and fixed some warnings

---
+++ test/test_hdf5_parallel.py
@@ -0,0 +1,54 @@
+# Unit and other tests
+import datetime
+import gdal
+import math
+import numpy as np
+import os
+import pandas as pd
+import unittest
+import scipy.io as sio
+import subprocess
+
+
+class hdf5_parallel_Tests(unittest.TestCase):
+
+    stepSize = 15.0
+    pnts_file = '../test/scenario_1/geom/query_points.h5'
+    wm_file = '../test/scenario_1/weather_files/ERA5_2020-01-03 23:00:00_15.75N_18.25N_-103.25E_-99.75E.h5'
+    interpType = 'rgi'
+    verbose = True
+    delayType = ['Zenith']
+    
+    from RAiDER.delayFcns import get_delays
+    
+    delays_wet_1, delays_hydro_1 = get_delays(stepSize, pnts_file, wm_file, interpType,
+                                              verbose, delayType, cpu_num = 1)
+    conts = sio.loadmat('get_delays_time_elapse.mat')
+    time_elapse_1 = conts['time_elapse'][0]
+    delays_wet_4, delays_hydro_4 = get_delays(stepSize, pnts_file, wm_file, interpType,
+                                              verbose, delayType, cpu_num = 4)
+    conts = sio.loadmat('get_delays_time_elapse.mat')
+    time_elapse_4 = conts['time_elapse'][0]
+    
+    subprocess.getoutput('rm get_delays_time_elapse.mat')
+
+    # test error messaging
+    def test_get_delays_wet_accuracy(self):
+        self.assertTrue(np.allclose(self.delays_wet_1, self.delays_wet_4))
+
+    
+    def test_get_delays_hydro_accuracy(self):
+        self.assertTrue(np.allclose(self.delays_hydro_1, self.delays_hydro_4))
+
+    
+    def test_get_delays_runtime(self):
+        self.assertTrue(self.time_elapse_1 > self.time_elapse_4)
+
+
+def main():
+    unittest.main()
+   
+if __name__=='__main__':
+
+    unittest.main()
+
