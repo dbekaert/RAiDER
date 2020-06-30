e67087a86fd64e9da608b4376596d53d1ebf20f4
sssangha <sssangha@ucla.edu>
2020-06-30 02:48:46 -0700

    Fix header typo

--- tools/bin/raiderDelay.py
+++ tools/bin/raiderDelay.py
@@ -1,4 +1,4 @@
-#!/u/leffe0/ssangha/tools/conda_installation/stable_feb9_2020/envs/RAiDER/bin/python3
+#!/usr/bin/env python3
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 # Author: Jeremy Maurer, Raymond Hogenson & David Bekaert
