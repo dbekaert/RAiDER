10f135e96ac3871ff3fd3e90d35818208db2fa50
Askaholic <askaholic907@gmail.com>
2020-06-30 15:20:40 -0800

    Name saved weathermodel without colons

--- test/test_utilFcns.py
+++ test/test_utilFcns.py
@@ -1,12 +1,12 @@
 # Unit and other tests
-import datetime
-import gdal
-import math
-import numpy as np
 import os
-import pandas as pd
-import pickle
 import unittest
+from datetime import time
+
+import gdal
+import numpy as np
+
+from RAiDER.utilFcns import makeDelayFileNames
 
 
 class FcnTests(unittest.TestCase):
@@ -14,19 +14,19 @@ class FcnTests(unittest.TestCase):
     # test sind
     theta1 = np.array([0,30,90,180])
     truev1 = np.array([0,0.5,1,0])
-    
+
     # test cosd
     theta2 = np.array([0,60,90,180])
     truev2 = np.array([1,0.5,0,-1])
-    
+
     # test tand
     theta3 = np.array([0,30,45,60])
     truev3 = np.array([0,1/np.sqrt(3),1,np.sqrt(3)])
-    
+
     # test gdal_open
     fname1 = '../test/test_geom/lat.rdr'
     shape1 = (45,226)
-    
+
     # test writeResultsToHDF5
     lats = np.array([15.0, 15.5, 16.0, 16.5, 17.5, -40, 60, 90])
     lons = np.array([-100.0, -100.4, -91.2, 45.0, 0., -100,-100, -100])
@@ -34,7 +34,7 @@ class FcnTests(unittest.TestCase):
     wet = np.zeros(lats.shape)
     hydro = np.ones(lats.shape)
     filename1 = 'dummy.hdf5'
-    
+
     # test writeArrayToRaster
     array = np.transpose(np.array([np.arange(0,10)]))*np.arange(0,10)
     filename2 = 'dummy.out'
@@ -87,7 +87,7 @@ class FcnTests(unittest.TestCase):
         test_result = makePoints1D(1000., np.array([0, 0, 0]), np.array([0, 0, 1]), 5)
         true_ray = np.stack([np.zeros((200,)), np.zeros((200,)), np.arange(0,1000,5)], axis = -1).T
         self.assertTrue(np.allclose(test_result, true_ray))
-  
+
     def test_makePoints3D_Python_dim(self):
         from RAiDER.utilFcns import makePoints3D
         sp = np.zeros((3, 3, 3, 3))
@@ -114,13 +114,13 @@ class FcnTests(unittest.TestCase):
         shape = (3,3,3,3,20)
         true_rays = df.reshape(shape)
         self.assertTrue(np.allclose(test_result, true_rays))
-  
+
     def test_makePoints1D_Cython(self):
         from RAiDER.makePoints import makePoints1D
         test_result = makePoints1D(1000., np.array([0., 0., 0.]), np.array([0., 0., 1.]), 5.)
         true_ray = np.stack([np.zeros((200,)), np.zeros((200,)), np.arange(0,1000,5)], axis = -1).T
         self.assertTrue(np.allclose(test_result, true_ray))
-  
+
     def test_makePoints3D_Cython_dim(self):
         from RAiDER.makePoints import makePoints3D
         sp = np.zeros((3, 3, 3, 3))
@@ -148,15 +148,41 @@ class FcnTests(unittest.TestCase):
         true_rays = df.reshape(shape)
         self.assertTrue(np.allclose(test_result, true_rays))
 
+    def test_makeDelayFileNames(self):
+        self.assertEqual(
+            makeDelayFileNames(None, None, "h5", "name", "dir"),
+            ("dir/name_wet_ztd.h5", "dir/name_hydro_ztd.h5")
+        )
+        self.assertEqual(
+            makeDelayFileNames(None, (), "h5", "name", "dir"),
+            ("dir/name_wet_std.h5", "dir/name_hydro_std.h5")
+        )
+        self.assertEqual(
+            makeDelayFileNames(time(1, 2, 3), None, "h5", "model_name", "dir"),
+            (
+                "dir/model_name_wet_01_02_03_ztd.h5",
+                "dir/model_name_hydro_01_02_03_ztd.h5"
+            )
+        )
+        self.assertEqual(
+            makeDelayFileNames(time(1, 2, 3), "los", "h5", "model_name", "dir"),
+            (
+                "dir/model_name_wet_01_02_03_std.h5",
+                "dir/model_name_hydro_01_02_03_std.h5"
+            )
+        )
+
+
 def test_cleanUp():
     import glob
     dummy_files = glob.glob('dummy' + '*')
     [os.remove(f) for f in dummy_files]
 
+
 def main():
     unittest.main()
-   
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
 
     unittest.main()
-
