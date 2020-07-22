e6408730dbc7872e95fa6905aac24e72bc1391c2
Askaholic <askaholic@protonmail.com>
2020-07-22 10:05:59 -0800

    Add logger with stdout handler

---
+++ tools/RAiDER/logger.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Author: Rohan Weeden
+# Copyright 2020, by the California Institute of Technology. ALL RIGHTS
+# RESERVED. United States Government Sponsorship acknowledged.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+Global logging configuration
+"""
+import logging
+import os
+import sys
+from logging import Formatter, StreamHandler
+
+
+# Inspired by
+# https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
+class UnixColorFormatter(Formatter):
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    COLORS = {
+        logging.WARNING: yellow,
+        logging.ERROR: red,
+        logging.CRITICAL: bold_red
+    }
+
+    def __init__(self, fmt=None, datefmt=None, style="%", use_color=True):
+        super().__init__(fmt, datefmt, style)
+        if use_color:
+            self.formatMessage = self.formatMessageColor
+
+    def formatMessageColor(self, record):
+        message = super().formatMessage(record)
+        color = self.COLORS.get(record.levelno)
+        if color:
+            return "".join([color, message, self.reset])
+        return message
+
+
+logger = logging.getLogger("RAiDER")
+logger.setLevel(logging.INFO)
+
+stdout_handler = StreamHandler(sys.stdout)
+stdout_handler.setFormatter(UnixColorFormatter(use_color=os.name != "nt"))
+
+logger.addHandler(stdout_handler)
