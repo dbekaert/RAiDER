d90f8ecb36b2fd5548baa04f8be523fc510c421e
Askaholic <askaholic@protonmail.com>
2020-09-02 11:33:15 -0800

    Use orbit file from tests

--- test/test_lookvectors.py
+++ test/test_lookvectors.py
@@ -39,7 +39,7 @@ def llh(lat, lon, hgt):
 
 @pytest.fixture
 def state_vector():
-    return read_ESA_Orbit_file(TEST_DIR / 'test_geom' / 'S1A_OPER_AUX_POEORB_OPOD_20200122T120701_V20200101T225942_20200103T005942.EOF')
+    return read_ESA_Orbit_file(TEST_DIR / 'los_files' / 'test_orbit.EOF')
 
 
 @pytest.fixture
@@ -103,17 +103,10 @@ def test_orbit_generator_simple(orbitgen, acq_time):
     # visually plotted them and they looked right, so we assume they are and
     # use this as a regression test so we know if the behaviour of the function
     # is changed.
-    assert np.allclose(ans, np.array([
-        [-0.05146681, -0.80103483, 0.59640119],
-        [+0.45607286, -0.58576985, 0.66997853],
-        [-0.46484434, -0.79222125, 0.39535456],
-        [+0.26247899, -0.63102352, 0.73000966],
-        [-0.22526362, -0.76458368, 0.60387756],
-        [-0.54500409, -0.73164134, 0.40947709],
-        [+0.55564835, -0.56916693, 0.60605603],
-        [+0.12338176, -0.81340164, 0.56846699],
-        [-0.26466444, -0.86499673, 0.42630199],
-    ]))
+    print(ans)
+    assert np.allclose(ans, np.tile(np.array(
+        [-0.04775827, -0.81398979,  0.57891258]
+    ), (9, 1)))
 
 
 @pytest.fixture(params=('zenithgen', 'orbitgen'))
