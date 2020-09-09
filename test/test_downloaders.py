6e2844a95999299153337bcd2032d716b11d3ef2
Jeremy Maurer <maurer.jeremy@gmail.com>
2020-09-09 14:27:42 -0500

    Fix file location in test_downloaders

--- test/test_downloaders.py
+++ test/test_downloaders.py
@@ -11,18 +11,18 @@ from RAiDER.models.erai import ERAI
 
 def test_era5():
     wm = ERA5()
-    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2020, 1, 1, 0, 0, 0), TEST_DIR / 'weather_files' / 'test_era5.nc')
+    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2020, 1, 1, 0, 0, 0), TEST_DIR / 'weather_model' / 'test_era5.nc')
 
 
 def test_era5t():
     wm = ERA5T()
-    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2020, 1, 1, 0, 0, 0), TEST_DIR / 'weather_files' / 'test_era5t.nc')
+    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2020, 1, 1, 0, 0, 0), TEST_DIR / 'weather_model' / 'test_era5t.nc')
 
 
 @pytest.mark.xfail(reason='ECMWF API is not working for some reason, need to revisit')
 def test_erai():
     wm = ERAI()
-    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2017, 1, 1, 0, 0, 0), TEST_DIR / 'weather_files' / 'test_erai.nc')
+    wm.fetch(np.array([10, 10.1, 10.2]), np.array([-72, -72, -72]), datetime(2017, 1, 1, 0, 0, 0), TEST_DIR / 'weather_model' / 'test_erai.nc')
 
 
 def test_erai2():
