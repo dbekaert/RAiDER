b6e74e6b0a2e62eb1f067423f72d522e5c922893
sssangha <simran.s.sangha@jpl.nasa.gov>
2020-11-05 12:35:11 -0800

    Change order of extents to WESN

--- tools/RAiDER/dem.py
+++ tools/RAiDER/dem.py
@@ -245,12 +245,13 @@ def getDEM(extent, out_dir):
     from shapely.geometry import shape
     from shapely.geometry import Polygon
 
+    # change order of extent to WESN
+    extent = [extent[2], extent[3], extent[0], extent[1]]
     # Get area of bounding box
     # use equal area projection centered on/bracketing AOI
     pa = Proj("+proj=aea +lat_1={} +lat_2={} +lat_0={} +lon_0={}".format(min(extent[1::2]), max(extent[1::2]), 
         (max(extent[1::2])+min(extent[1::2]))/2, (max(extent[::2])+min(extent[::2]))/2))
     # Use shapely to get coordinates along box
-    print('extent!!!!', extent)
     bbox = Polygon(np.column_stack((np.array([min(extent[::2]),max(extent[::2]),max(extent[::2]),min(extent[::2]),min(extent[::2])]), 
         np.array([min(extent[1::2]),min(extent[1::2]),max(extent[1::2]),max(extent[1::2]),min(extent[1::2])]))))
     lon, lat = bbox.exterior.coords.xy
