6c91397958597590bc8bc6e10b0157dd1a56f4e8
Jeremy Maurer <maurer.jeremy@gmail.com>
2020-12-15 11:59:36 -0600

    close plots after saving (#228)
    * close plots after saving
    
    * Add matplotlib to imports

--- tools/RAiDER/processWM.py
+++ tools/RAiDER/processWM.py
@@ -11,6 +11,7 @@ import os
 import sys
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from datetime import datetime, date
 
@@ -119,5 +120,6 @@ def prepareWeatherModel(
     if makePlots:
         p = weather_model.plot('wh', True)
         p = weather_model.plot('pqt', True)
+        plt.close('all')
 
     return weather_model, lats, lons
