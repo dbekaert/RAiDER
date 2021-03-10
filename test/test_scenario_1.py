23a8df6f5c205c15ee2f67025de384cb43022d66
Yang Lei <leiyangfrancis@gmail.com>
2021-03-09 17:42:42 -0800

    add all other models besides ERA5 to test scenario #1

--- test/test_scenario_1.py
+++ test/test_scenario_1.py
@@ -12,11 +12,60 @@ from RAiDER.utilFcns import gdal_open, makeDelayFileNames, modelName2Module
 SCENARIO_DIR = os.path.join(TEST_DIR, "scenario_1")
 _RTOL = 1e-4
 
+def test_tropo_delay_ERA5(tmp_path):
+    '''
+    Scenario:
+    1: Small area, ERA5, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="ERA5")
 
-def test_tropo_delay(tmp_path):
+def test_tropo_delay_GMAO(tmp_path):
+    '''
+    Scenario:
+    1: Small area, GMAO, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="GMAO")
+
+def test_tropo_delay_MERRA2(tmp_path):
+    '''
+    Scenario:
+    1: Small area, MERRA2, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="MERRA2")
+
+def test_tropo_delay_HRES(tmp_path):
+    '''
+    Scenario:
+    1: Small area, HRES, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="HRES")
+
+def test_tropo_delay_ERA5T(tmp_path):
+    '''
+    Scenario:
+    1: Small area, ERA5T, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="ERA5T")
+
+def test_tropo_delay_ERAI(tmp_path):
+    '''
+    Scenario:
+    1: Small area, ERAI, Zenith delay
+    '''
+    core_test_tropo_delay(tmp_path, modelName="ERAI")
+
+#def test_tropo_delay_NCMR(tmp_path):
+#    '''
+#    Scenario:
+#    1: Small area, NCMR, Zenith delay
+#    '''
+#    core_test_tropo_delay(tmp_path, modelName="NCMR")
+
+
+def core_test_tropo_delay(tmp_path, modelName):
     '''
     Scenario: 
-    1: Small area, ERA5, Zenith delay
+    1: Small area, Zenith delay
     '''
     lats = gdal_open(os.path.join(
         SCENARIO_DIR, 'geom', 'lat.dat'
@@ -24,16 +73,21 @@ def test_tropo_delay(tmp_path):
     lons = gdal_open(os.path.join(
         SCENARIO_DIR, 'geom', 'lon.dat'
     ))
-
-    time = datetime.datetime(2020, 1, 3, 23, 0)
+    
+    if modelName == 'ERAI':
+        time = datetime.datetime(2018, 1, 3, 23, 0)
+    elif modelName == 'NCMR':
+        time = datetime.datetime(2018, 7, 1, 0, 0)
+    else:
+        time = datetime.datetime(2020, 1, 3, 23, 0)
 
     wmLoc = os.path.join(SCENARIO_DIR, 'weather_files')
     if not os.path.exists(wmLoc):
         os.mkdir(wmLoc)
 
-    _, model_obj = modelName2Module("ERA5")
+    _, model_obj = modelName2Module(modelName)
     wet_file, hydro_file = makeDelayFileNames(
-        time, Zenith, "envi", "ERA5", tmp_path
+        time, Zenith, "envi", modelName, tmp_path
     )
 
     with pushd(tmp_path):
@@ -44,9 +98,8 @@ def test_tropo_delay(tmp_path):
         args['lons'] = lons
         args['ll_bounds'] = (15.75, 18.25, -103.24, -99.75)
         args['heights'] = ("dem", os.path.join(TEST_DIR, "test_geom", "warpedDEM.dem"))
-        args['pnts_file'] = 'lat_query_points.h5'
         args['flag'] = "files"
-        args['weather_model'] = {"type": model_obj(), "files": None, "name": "ERA5"}
+        args['weather_model'] = {"type": model_obj(), "files": None, "name": modelName}
         args['wmLoc'] = wmLoc
         args['zref'] = 20000.
         args['outformat'] = "envi"
@@ -64,14 +117,14 @@ def test_tropo_delay(tmp_path):
         true_wet = gdal_open(
             os.path.join(
                 SCENARIO_DIR,
-                "wet.envi"
+                modelName+"/wet.envi"
             ),
             userNDV=0.
         )
         true_hydro = gdal_open(
             os.path.join(
                 SCENARIO_DIR,
-                "hydro.envi"
+                modelName+"/hydro.envi"
             ),
             userNDV=0.
         )
