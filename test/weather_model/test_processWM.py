9c27e2c60701160427330aaad212465cd999a72d
cmarshak <charles.marshak@gmail.com>
2021-03-17 16:32:50 -0700

    include bounds of weather model in test

--- test/weather_model/test_processWM.py
+++ test/weather_model/test_processWM.py
@@ -14,6 +14,13 @@ SCENARIO_DIR = os.path.join(TEST_DIR, "scenario_1")
 
 @pytest.fixture
 def getWM():
+    """The bounds of the ERA model are:
+
+    BoundingBox(left=-103.7750015258789,
+                bottom=15.225000381469727,
+                right=-99.2750015258789,
+                top=18.725000381469727)
+    """
     wm = ERA5()
     wm.files = [
         os.path.join(
