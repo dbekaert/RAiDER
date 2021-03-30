3814677bccdb475407b41f26bdc5b47719496025
Jeremy Maurer <jlmaurer@stanford.edu>
2021-03-30 07:07:37 -0500

    Start adding unit tests to improve coverage

--- test/test_util.py
+++ test/test_util.py
@@ -12,8 +12,11 @@ from osgeo import gdal, osr
 from RAiDER.utilFcns import (
     _least_nonzero, cosd, gdal_open, makeDelayFileNames, sind,
     writeArrayToRaster, writeResultsToHDF5, gdal_extents, modelName2Module,
-    getTimeFromFile
+    getTimeFromFile, read_wm_file
 )
+
+
+SCENARIO_DIR = os.path.join(TEST_DIR, "scenario_1")
 
 
 @pytest.fixture
@@ -352,3 +355,15 @@ def test_WGS84_to_UTM():
     cal_utm_common = np.array([Z, X, Y]).transpose()
     assert np.allclose(true_utm_common, cal_utm_common)
     assert np.all(true_utm_common_letter == L)
+
+
+def test_read_weather_model_file():
+    weather_model_obj = read_wm_file(
+            os.path.join(
+                SCENARIO_DIR,
+                'weather_files',
+                'ERA5_2020_01_03_T23_00_00_15.75N_18.25N_103.24W_99.75W.nc'
+            )
+        )
+    assert weather_model_obj.Model() == 'ERA-5'
+
