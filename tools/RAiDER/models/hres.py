5e9fe8c2aa8f87962d891d80815af36c7eef963c
Jeremy Maurer <maurer.jeremy@gmail.com>
2021-04-15 22:26:21 -0500

    For some reason ECMWF doesn't like lat_/lon_min to be arbitrary floats

--- tools/RAiDER/models/hres.py
+++ tools/RAiDER/models/hres.py
@@ -12,6 +12,8 @@ from RAiDER.models.model_levels import (
     LEVELS_91_HRES,
     A_137_HRES,
     B_137_HRES,
+    A_91_HRES,
+    B_91_HRES,
 )
 
 
@@ -68,7 +70,6 @@ class HRES(WeatherModel):
     def __model_levels__(self):
         self._levels = 137
         self._zlevels = LEVELS_137_HEIGHTS
-
         self._a = A_137_HRES
         self._b = B_137_HRES 
 
@@ -77,34 +78,8 @@ class HRES(WeatherModel):
         # based on https://www.ecmwf.int/en/forecasts/documentation-and-support/91-model-levels
         self._levels = 91
         self._zlevels = LEVELS_91_HEIGHTS
-
-        self._a = [0.000000, 2.000040, 3.980832, 7.387186, 12.908319, 21.413612, 33.952858,
-                   51.746601, 76.167656, 108.715561, 150.986023, 204.637451, 271.356506,
-                   352.824493, 450.685791, 566.519226, 701.813354, 857.945801, 1036.166504,
-                   1237.585449, 1463.163940, 1713.709595, 1989.874390, 2292.155518, 2620.898438,
-                   2976.302246, 3358.425781, 3767.196045, 4202.416504, 4663.776367, 5150.859863,
-                   5663.156250, 6199.839355, 6759.727051, 7341.469727, 7942.926270, 8564.624023,
-                   9208.305664, 9873.560547, 10558.881836, 11262.484375, 11982.662109, 12713.897461,
-                   13453.225586, 14192.009766, 14922.685547, 15638.053711, 16329.560547, 16990.623047,
-                   17613.281250, 18191.029297, 18716.968750, 19184.544922, 19587.513672, 19919.796875,
-                   20175.394531, 20348.916016, 20434.158203, 20426.218750, 20319.011719, 20107.031250,
-                   19785.357422, 19348.775391, 18798.822266, 18141.296875, 17385.595703, 16544.585938,
-                   15633.566406, 14665.645508, 13653.219727, 12608.383789, 11543.166992, 10471.310547,
-                   9405.222656, 8356.252930, 7335.164551, 6353.920898, 5422.802734, 4550.215820, 3743.464355,
-                   3010.146973, 2356.202637, 1784.854614, 1297.656128, 895.193542, 576.314148, 336.772369,
-                   162.043427, 54.208336, 6.575628, 0.003160, 0.000000]
-
-        self._b = [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
-                   0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
-                   0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000,
-                   0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000014,
-                   0.000055, 0.000131, 0.000279, 0.000548, 0.001000, 0.001701, 0.002765, 0.004267, 0.006322,
-                   0.009035, 0.012508, 0.016860, 0.022189, 0.028610, 0.036227, 0.045146, 0.055474, 0.067316,
-                   0.080777, 0.095964, 0.112979, 0.131935, 0.152934, 0.176091, 0.201520, 0.229315, 0.259554,
-                   0.291993, 0.326329, 0.362203, 0.399205, 0.436906, 0.475016, 0.513280, 0.551458, 0.589317,
-                   0.626559, 0.662934, 0.698224, 0.732224, 0.764679, 0.795385, 0.824185, 0.850950, 0.875518,
-                   0.897767, 0.917651, 0.935157, 0.950274, 0.963007, 0.973466, 0.982238, 0.989153, 0.994204,
-                   0.997630, 1.000000]
+        self._a = A_91_HRES
+        self._b = B_91_HRES
 
 
     def load_weather(self, filename=None):
@@ -249,40 +224,19 @@ class HRES(WeatherModel):
             self.update_a_b()
 
         # execute the search at ECMWF
-        if self._level_type == 'ml':
-            self._download_ecmwf_file(lat_min, lat_max, self._lat_res, lon_min, lon_max, self._lon_res, time, out)
+        self._download_ecmwf(lat_min, lat_max, self._lat_res, lon_min, lon_max, self._lon_res, time, out)
+
+    def _download_ecmwf(self, lat_min, lat_max, lat_step, lon_min, lon_max, lon_step, time, out):
+        from ecmwfapi import ECMWFService
+
+        server = ECMWFService("mars")
+
+        corrected_date = round_date(time, datetime.timedelta(hours=6))
+
+        if self._model_level_type == 'ml':
+            param = "129/130/133/152"
         else:
-            self._download_ecmwf_pl(lat_min, lat_max, self._lat_res, lon_min, lon_max, self._lon_res, time, out)
-
-    def _download_ecmwf_file(self, lat_min, lat_max, lat_step, lon_min, lon_max, lon_step, time, out):
-        from ecmwfapi import ECMWFService
-
-        server = ECMWFService("mars")
-
-        corrected_date = round_date(time, datetime.timedelta(hours=6))
-
-        server.execute({
-            'class': self._classname,
-            'date': datetime.datetime.strftime(corrected_date, "%Y-%m-%d"),
-            'expver': "{}".format(self._expver),
-            'levelist': "1/to/{0}".format(self._levels),
-            'levtype': "ml",
-            'param': "129/130/133/152",
-            'stream': "oper",
-            'time': "{}".format(datetime.time.strftime(corrected_date.time(), '%H:%M')),
-            'type': "an",
-            'step': "0",
-            'grid': "{}/{}".format(lon_step, lat_step),
-            'area': "{}/{}/{}/{}".format(lat_max, lon_min, lat_min, lon_max),
-            'format': "netcdf", },
-            out)
-
-    def _download_ecmwf_pl(self, lat_min, lat_max, lat_step, lon_min, lon_max, lon_step, time, out):
-        from ecmwfapi import ECMWFService
-
-        server = ECMWFService("mars")
-
-        corrected_date = round_date(time, datetime.timedelta(hours=6))
+            param = "129.128/130.128/133.128/152"
 
         server.execute(
             {
@@ -294,12 +248,12 @@ class HRES(WeatherModel):
                 'type': "an",
                 'levelist': "all",
                 'levtype': "{}".format(self._model_level_type),
-                'param': "129.128/130.128/133.128/152",
+                'param': param,
                 'date': datetime.datetime.strftime(corrected_date, "%Y-%m-%d"),
-                'time': "{}".format(datetime.time.strftime(corrected_date.time(), '%H:%M:%S')),
+                'time': "{}".format(datetime.time.strftime(corrected_date.time(), '%H:%M')),
                 'step': "0",
                 'grid': "{}/{}".format(lon_step, lat_step),
-                'area': "{}/{}/{}/{}".format(lat_max, lon_min, lat_min, lon_max),
+                'area': "{}/{}/{}/{}".format(lat_max, floorish(lon_min, 0.1), floorish(lat_min, 0.1), lon_max),
                 'format': "netcdf",
             },
             out
@@ -378,3 +332,8 @@ class HRES(WeatherModel):
         self._p = np.flip(self._p, axis=2)
         self._t = np.flip(self._t, axis=2)
         self._q = np.flip(self._q, axis=2)
+
+def floorish(val, frac):
+    '''Round a value to the lower fractional part'''
+    return val - (val % frac)
+
