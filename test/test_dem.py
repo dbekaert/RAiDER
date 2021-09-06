55a3482223b9c876bf829a31e5b3e122a64caaab
Jeremy Maurer <maurer.jeremy@gmail.com>
2021-09-06 14:47:18 -0500

    remove DEM download from test suite

--- test/test_dem.py
+++ test/test_dem.py
@@ -6,7 +6,7 @@ import numpy as np
 from test import DATA_DIR, pushd
 
 from RAiDER.dem import (
-    getBufferedExtent, isOutside, isInside, getDEM, forceNDArray
+    getBufferedExtent, isOutside, isInside, forceNDArray
 )
 
 
@@ -72,11 +72,6 @@ def test_isInside(llsimple):
 
 def test_isInside(llsimple):
     assert not isInside(getBufferedExtent(*llsimple), getBufferedExtent(*llsimple) + 1)
-
-
-def test_getDEM(tmp_path):
-    with pushd(tmp_path):
-        getDEM([18.5, 18.9, -73.2, -72.8], tmp_path)
 
 
 def test_isNDArray():
