939390872704801c70daeaa7493f5c3585abcc45
Jeremy Maurer <maurer.jeremy@gmail.com>
2021-09-06 22:49:31 -0500

    Take care of GNSS duplicates

--- tools/RAiDER/checkArgs.py
+++ tools/RAiDER/checkArgs.py
@@ -125,7 +125,7 @@ def checkArgs(args, p):
             hydroFilename = wetFilename
 
             # copy the input file to the output location for editing
-            indf = pd.read_csv(args.query_area)
+            indf = pd.read_csv(args.query_area).group_by(by=["Lat", "Lon"]).first()
             indf.to_csv(wetFilename, index=False)
         else:
             wetFilename, hydroFilename = makeDelayFileNames(
