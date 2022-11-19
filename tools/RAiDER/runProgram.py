2a2dc21ee0c4e45aa1949fb8d5446692ac5b9159
bbuzz31 <bbuzz318@icloud.com>
2022-11-18 17:37:53 -0800

    download GNSS stations works

--- tools/RAiDER/runProgram.py
+++ tools/RAiDER/runProgram.py
@@ -28,9 +28,14 @@ def main(iargs=None):
 
 
     if 'download_gnss' in step_list:
-        # from RAiDER.downloadGNSSDelays import main
-        # main(params)
-        pass
+        from RAiDER.downloadGNSSDelays import main
+        params['gps_repo'] = 'UNR' # only UNR supported; used to be exposed
+        params['out']      = 'products' # output directory
+        params['download'] = False
+        params['cpus']     = 4
+        params['bounding_box'] = params['aoi'].bounds()
+
+        main(params)
 
 
     if 'load_weather_model' in step_list or 'calculate_delays' in step_list:
