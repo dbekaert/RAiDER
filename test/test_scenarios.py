f77b0b48e71c69eb7ec170d48e656817f21ef118
Jeremy Maurer <jmaurer@mst.edu>
2022-12-01 16:58:03 -0600

    take out conventional slant for now

--- test/test_scenarios.py
+++ test/test_scenarios.py
@@ -34,13 +34,4 @@ def test_scenario_3():
     subprocess.run(['rm', '-f', './HRRR*'])
     subprocess.run(['rm', '-rf', './weather_files'])
 
-@pytest.mark.long
-def test_scenario_4():
-    test_path = os.path.join(TEST_DIR, "scenario_3", 'raider_example_4.yaml')
-    process = subprocess.run(['raider.py', test_path],stdout=subprocess.PIPE, universal_newlines=True)
-    assert process.returncode == 0
-
-    # Clean up files
-    subprocess.run(['rm', '-f', './HRRR*'])
-    subprocess.run(['rm', '-rf', './weather_files'])
 
