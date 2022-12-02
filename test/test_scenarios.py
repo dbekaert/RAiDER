a8c452ac76750ad365e226aebaa1cd4c3931ac16
Jeremy Maurer <jmaurer@mst.edu>
2022-12-01 21:20:48 -0600

    update golden datasets paths

--- test/test_scenarios.py
+++ test/test_scenarios.py
@@ -7,18 +7,16 @@ from test import TEST_DIR
 import numpy as np
 import xarray as xr
 
-GD_SRC_DIR = './'
-
 
 @pytest.mark.long
 def test_scenario_1():
-    test_path = os.path.join(TEST_DIR, "scenario_1", 'raider_example_1.yaml')
+    SCENARIO_DIR = os.path.join(TEST_DIR, "scenario_1")
+    test_path = os.path.join(SCENARIO_DIR, 'raider_example_1.yaml')
     process = subprocess.run(['raider.py', test_path],stdout=subprocess.PIPE, universal_newlines=True,)
     assert process.returncode == 0
 
     new_data = xr.load_dataset('HRRR_tropo_20200101T120000_ztd.nc')
-
-    golden_data = xr.load_dataset(os.path.join(GD_SRC_DIR, 'HRRR_tropo_20200101T120000_ztd.nc'))
+    golden_data = xr.load_dataset(os.path.join(SCENARIO_DIR, 'HRRR_tropo_20200101T120000_ztd.nc'))
 
     assert np.allclose(golden_data['wet'], new_data['wet'])
     assert np.allclose(golden_data['hydro'], new_data['hydro'])
@@ -27,6 +25,7 @@ def test_scenario_1():
     # Clean up files
     subprocess.run(['rm', '-f', './HRRR*'])
     subprocess.run(['rm', '-rf', './weather_files'])
+
 
 #TODO: Next release include GNSS station test
 # @pytest.mark.long
@@ -39,17 +38,17 @@ def test_scenario_1():
 #     subprocess.run(['rm', '-f', './HRRR*'])
 #     subprocess.run(['rm', '-rf', './weather_files'])
 
+
 @pytest.mark.long
 def test_scenario_3():
-    test_path = os.path.join(TEST_DIR, "scenario_3", 'raider_example_3.yaml')
+    SCENARIO_DIR = os.path.join(TEST_DIR, "scenario_3")
+
+    test_path = os.path.join(SCENARIO_DIR, 'raider_example_3.yaml')
     process = subprocess.run(['raider.py', test_path],stdout=subprocess.PIPE, universal_newlines=True)
     assert process.returncode == 0
 
     new_data = xr.load_dataset('HRRR_tropo_20181113T230000_std.nc')
-
-
-    golden_data = xr.load_dataset(os.path.join(GD_SRC_DIR, 'HRRR_tropo_20181113T230000_std.nc'))
-
+    golden_data = xr.load_dataset(os.path.join(SCENARIO_DIR, 'HRRR_tropo_20181113T230000_std.nc'))
 
     assert np.allclose(golden_data['wet'], new_data['wet'])
     assert np.allclose(golden_data['hydro'], new_data['hydro'])
@@ -57,5 +56,3 @@ def test_scenario_3():
     # Clean up files
     subprocess.run(['rm', '-f', './HRRR*'])
     subprocess.run(['rm', '-rf', './weather_files'])
-
-
