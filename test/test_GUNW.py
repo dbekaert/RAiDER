4a3abda5bceba012ff049971a9ee23aaa728a61c
bbuzz31 <bbuzz318@icloud.com>
2022-12-15 15:56:33 -0800

    fix paths in GUNW test

---
+++ test/test_GUNW.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+import numpy as np
+import pytest
+import shutil
+import subprocess
+
+from test import TEST_DIR
+
+
+def test_GUNW():
+    ## eventually to be implemented
+    # home = os.path.expanduser('~')
+    # netrc = os.path.join(home, '.netrc')
+# 
+    # ## make netrc
+    # if not os.path.exists(netrc):
+        # name, passw = os.getenv('URSname'), os.getenv('URSpass')
+        # cmd = f'echo "machine urs.earthdata.nasa.gov login {name} password {passw}" > ~/.netrc'
+        # subprocess.run(cmd.split())
+# 
+        # cmd = f'chmod 600 {netrc}'
+        # subprocess.run(cmd.split())
+# 
+    SCENARIO_DIR = os.path.join(TEST_DIR, "GUNW")
+    os.makedirs(SCENARIO_DIR, exist_ok=True)
+    GUNW = 'S1-GUNW-D-R-071-tops-20200130_20200124-135156-34956N_32979N-PP-913f-v2_0_4.nc'
+    orig_GUNW = os.path.join(TEST_DIR, GUNW)
+    updated_GUNW = os.path.join(SCENARIO_DIR, GUNW)
+    shutil.copy(orig_GUNW, updated_GUNW)
+
+
+    # cmd  = f'wget https://grfn.asf.alaska.edu/door/download/{GUNW}'
+    # proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
+    # assert np.isclose(proc.returncode, 0)
+
+    cmd  = f'raider.py ++process calcDelaysGUNW {updated_GUNW} -m ERA5'
+    proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
+    assert np.isclose(proc.returncode, 0)
+
+    # Clean up files
+    # shutil.rmtree(SCENARIO_DIR)
+
