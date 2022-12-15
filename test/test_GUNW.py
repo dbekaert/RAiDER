494992e909beb4148f21c369954e85276d0a2e26
bbuzz31 <bbuzz318@icloud.com>
2022-12-14 18:21:49 -0800

    Temporarily add GUNW file to test dir

--- test/test_GUNW.py
+++ test/test_GUNW.py
@@ -2,31 +2,36 @@ import os
 import shutil
 import numpy as np
 import pytest
+import shutil
 import subprocess
 
 from test import TEST_DIR
 
 
 def test_GUNW():
-    home = os.path.expanduser('~')
-    netrc = os.path.join(home, '.netrc')
-
-    ## make netrc
-    if not os.path.exists(netrc):
-        name, passw = os.getenv('URSname'), os.getenv('URSpass')
-        cmd = f'echo "machine urs.earthdata.nasa.gov login {name} password {passw}" > ~/.netrc'
-        subprocess.run(cmd.split())
-
-        cmd = f'chmod 600 {netrc}'
-        subprocess.run(cmd.split())
-
+    ## eventually to be implemented
+    # home = os.path.expanduser('~')
+    # netrc = os.path.join(home, '.netrc')
+# 
+    # ## make netrc
+    # if not os.path.exists(netrc):
+        # name, passw = os.getenv('URSname'), os.getenv('URSpass')
+        # cmd = f'echo "machine urs.earthdata.nasa.gov login {name} password {passw}" > ~/.netrc'
+        # subprocess.run(cmd.split())
+# 
+        # cmd = f'chmod 600 {netrc}'
+        # subprocess.run(cmd.split())
+# 
     SCENARIO_DIR = os.path.join(TEST_DIR, "GUNW")
     os.makedirs(SCENARIO_DIR, exist_ok=True)
     os.chdir(SCENARIO_DIR)
     GUNW = 'S1-GUNW-D-R-071-tops-20200130_20200124-135156-34956N_32979N-PP-913f-v2_0_4.nc'
-    cmd  = f'wget https://grfn.asf.alaska.edu/door/download/{GUNW}'
-    proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
+    shutil.copy(f'../{GUNW}', os.getcwd())
+
+
+    # cmd  = f'wget https://grfn.asf.alaska.edu/door/download/{GUNW}'
+    # proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
+    # assert np.isclose(proc.returncode, 0)
 
     cmd  = f'raider.py ++process calcDelaysGUNW {GUNW}'
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
@@ -34,5 +39,5 @@ def test_GUNW():
 
     # Clean up files
     os.chdir('..')
-    shutil.rmtree(SCENARIO_DIR)
+    # shutil.rmtree(SCENARIO_DIR)
 
