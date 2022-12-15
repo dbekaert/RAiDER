74c598e7a607e8cd828f65d0f22bcc55f06f71b4
bbuzz31 <bbuzz318@icloud.com>
2022-12-15 14:27:06 -0800

    dummy for circleci

--- tools/RAiDER/__init__.py
+++ tools/RAiDER/__init__.py
@@ -8,4 +8,4 @@ from importlib.metadata import version
 __version__ = version(__name__)
 __copyright__ = 'Copyright (c) 2019-2022, California Institute of Technology ("Caltech"). All rights reserved.'
 
-__all__ = ['__version__', '__copyright__']
+__all__ = ['__version__', '__copyright__', 'hi']
