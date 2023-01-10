5655b98ef7df98e1904f94c13c93be09235269ac
bbuzz31 <bbuzz318@icloud.com>
2023-01-10 11:50:04 -0800

    few updates to validators

--- tools/RAiDER/cli/validators.py
+++ tools/RAiDER/cli/validators.py
@@ -61,11 +61,10 @@ def get_heights(args, out, station_file, bounding_box=None):
     '''
     Parse the Height info and download a DEM if needed
     '''
-    dem_path = os.path.join(out, 'geom')
-    if not os.path.exists(dem_path):
-        os.mkdir(dem_path)
+    dem_path = out
+
     out = {
-            'dem': None,
+            'dem': args.get('dem'),
             'height_file_rdr': None,
             'height_levels': None,
         }
@@ -75,7 +74,7 @@ def get_heights(args, out, station_file, bounding_box=None):
             if 'Hgt_m' not in pd.read_csv(station_file):
                 out['dem'] = os.path.join(dem_path, 'GLO30.dem')
         elif os.path.exists(args.dem):
-            out['dem'] = args['dem']
+            out['dem'] = os.path.join(out, 'GLO30.dem')
             if bounding_box is not None:
                 dem_bounds = rio_extents(rio_profile(args.dem))
                 lats = dem_bounds[:2]
