8e59c45d97c77e8104304108f120c3f995fefa7c
bbuzz31 <bbuzz318@icloud.com>
2023-03-01 18:38:46 -0800

    update log message

--- tools/RAiDER/models/gmao.py
+++ tools/RAiDER/models/gmao.py
@@ -72,7 +72,7 @@ class GMAO(WeatherModel):
         time1 = time
         time = round_time(time, self._time_res * 60 * 60)
         if not time1 == time:
-            logger.warning('Rounded given hour from  %d to %d. May be incorrect near beginning/end of day.', time1.hour, time.hour)
+            logger.warning('Rounded given datetime from  %s to %s', time1, time)
 
         DT = time - T0
         time_ind = int(DT.total_seconds() / 3600.0 / 3.0)
