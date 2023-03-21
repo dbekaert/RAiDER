26c24b094299ecae74b3c74fa848df059221b67f
Jeremy Maurer <jmaurer@mst.edu>
2023-03-20 21:26:43 -0500

    initial try and refacting the hrrr code to use HERBIE to pull proj and data

--- tools/RAiDER/models/hrrr.py
+++ tools/RAiDER/models/hrrr.py
@@ -78,10 +78,7 @@ class HRRR(WeatherModel):
         '''
         Fetch weather model data from HRRR
         '''
-        # bounding box plus a buffer
-        time = self._time
-
-        self.files = self._download_hrrr_file(time, out)
+        self._download_hrrr_file(self._time, out)
 
 
     def load_weather(self, *args, filename=None, **kwargs):
@@ -106,8 +103,6 @@ class HRRR(WeatherModel):
         temps = ds['t'].values.transpose(1, 2, 0)
         qs = ds['q'].values.transpose(1, 2, 0)
         geo_hgt = ds['z'].values.transpose(1, 2, 0)
-
-        Ny, Nx = lats.shape
 
         lons[lons > 180] -= 360
 
@@ -134,152 +129,82 @@ class HRRR(WeatherModel):
         self._lons = _lons
 
 
-    def _makeDataCubes(self, filename, out=None):
-        '''
-        Create a cube of data representing temperature and relative humidity
-        at specified pressure levels
-        '''
-        if out is None:
-            out = filename
-
-        # Get profile information from gdal
-        prof = rio_profile(str(filename))
-
-        # Now get bounds
-        S, N, W, E = self._ll_bounds
-
-        # open the dataset and pull the data
-        ds = xarray.open_dataset(filename, engine='cfgrib', filter_by_keys={'typeOfLevel': 'isobaricInhPa'})
-
-        # Determine mask based on query bounds
-        lats = ds["latitude"].to_numpy()
-        lons = ds["longitude"].to_numpy()
-        levels = ds["isobaricInhPa"].to_numpy()
-        shp = lats.shape
-        lons[lons > 180.0] -= 360.
-        m1 = (S <= lats) & (N >= lats) &\
-                (W <= lons) & (E >= lons)
-
-        if np.sum(m1) == 0:
-            raise RuntimeError('Area of Interest has no overlap with the HRRR model available extent')
-
-        # Y extent
-        m1_y = np.argwhere(np.sum(m1, axis=1) != 0)
-        y_min = max(m1_y[0][0] - 2, 0)
-        y_max = min(m1_y[-1][0] + 3, shp[0])
-        m1_y = None
-
-        # X extent
-        m1_x = np.argwhere(np.sum(m1, axis=0) != 0)
-        x_min = max(m1_x[0][0] - 2, 0)
-        x_max = min(m1_x[-1][0] + 3, shp[1])
-        m1_x = None
-        m1 = None
-
-        # Coordinate arrays
-        # HRRR GRIB has data in south-up format
-        trans = prof["transform"].to_gdal()
-        xArr = trans[0] + (np.arange(x_min, x_max) + 0.5) * trans[1]
-        yArr = trans[3] + (prof["height"] * trans[5]) - (np.arange(y_min, y_max) + 0.5) * trans[5]
-        lats = lats[y_min:y_max, x_min:x_max]
-        lons = lons[y_min:y_max, x_min:x_max]
-
-        # Data variables
-        t = ds['t'][:, y_min:y_max, x_min:x_max].to_numpy()
-        z = ds['gh'][:, y_min:y_max, x_min:x_max].to_numpy()
-        q = ds['q'][:, y_min:y_max, x_min:x_max].to_numpy()
-        ds.close()
-
-        # This section is purely for flipping arrays as needed
-        # to match ECMWF reader is doing
-        # All flips are views - no extra memory use
-        # Lon -> From west to east
-        # Lat -> From south to north (ECMWF reads north to south and flips it
-        # load_weather) - So we do south to north here
-        # Pres -> High to Los - (ECWMF does now to high and flips it back) - so
-        # we do high to low
-        # Data is currently in [levels, y, x] order
-        flip_axes = []
-        if levels[-1] > levels[0]:
-            flip_axes.append(0)
-            levels = np.flip(levels)
-
-        if lats[0, 0] > lats[-1, 0]:
-            flip_axes.append(1)
-            lats = np.flip(lats, 0)
-            yArr = np.flip(yArr)
-
-        if lons[0, 0] > lons[0, -1]:
-            flip_axes.append(2)
-            lons = np.flip(lons, 1)
-            xArr = np.flip(xArr)
-
-        flip_axes = tuple(flip_axes)
-        if flip_axes:
-            t = np.flip(t, flip_axes)
-            z = np.flip(z, flip_axes)
-            q = np.flip(q, flip_axes)
-
-        # Create output dataset
-        ds_new = xarray.Dataset(
-            data_vars=dict(
-                t= (["level", "y", "x"], t,
-                    {"grid_mapping": "proj"}),
-                z= (["level", "y", "x"], z,
-                    {"grid_mapping": "proj"}),
-                q= (["level", "y", "x"], q,
-                    {"grid_mapping": "proj"}),
-                lats=(["y", "x"], lats),
-                lons=(["y", "x"], lons),
-            ),
-            coords=dict(
-                levels=(["level"], levels,
-                       {"units": "millibars",
-                        "long_name":  "pressure_level",
-                        "axis": "Z"}),
-                x=(["x"], xArr,
-                   {"standard_name": "projection_x_coordinate",
-                    "units": "m",
-                    "axis": "X"}),
-                y=(["y"], yArr,
-                   {"standard_name": "projection_y_coordinate",
-                    "units": "m",
-                    "axis": "Y"}),
-            ),
-            attrs={
-                'Conventions': 'CF-1.7',
-                'Weather_model':'HRRR',
-           }
-        )
-
-        # Write projection of output
-        ds_new["proj"] = int()
-        for k, v in self._proj.to_cf().items():
-            ds_new.proj.attrs[k] = v
-
-        ds_new.to_netcdf(out, engine='netcdf4')
-
-
     def _download_hrrr_file(self, DATE, out, model='hrrr', product='prs', fxx=0, verbose=False):
         '''
         Download a HRRR model
         '''
         ## TODO: Check how Herbie does temporal interpolation
-        # corrected_DT = round_date(DATE, datetime.timedelta(hours=self._time_res))
-        # if not corrected_DT == DATE:
-        #     logger.warning('Rounded given datetime from  %s to %s', DATE, corrected_DT)
-        corrected_DT = DATE
-
         H = Herbie(
-            corrected_DT.strftime('%Y-%m-%d %H:%M'),
+            DATE.strftime('%Y-%m-%d %H:%M'),
             model=model,
             product=product,
+            fxx=fxx,
             overwrite=False,
             verbose=True,
             save_dir=Path(os.path.dirname(out)),
         )
-        pf = H.download(":(SPFH|PRES|TMP|HGT):", verbose=verbose)
 
-        self._makeDataCubes(pf, out)
+        # Iterate through the list of datasets
+        #NOTE: this will need to be modified once HERBIE incorporates the
+        # xarray datatree: https://github.com/blaylockbk/Herbie/issues/146
+        ds_list = H.xarray(":(SPFH|PRES|TMP|HGT):", verbose=verbose)
+        ds_out = None
+        for ds in ds_list:
+            for var in ds.data_vars:
+                if var in ['t', 'q', 'gh']:
+                    if (len(ds[var].shape) == 3) & (ds[var].shape[0] > 2):
+                        ds_out = ds
+                        break
+        assert ds_out is not None
 
-        return out
+        # bookkeepping
+        ds_out = ds_out.assign_coords(longitude=(((ds_out.longitude + 180) % 360) - 180))
+        ds_out = ds_out.rename({'gh': 'z', 'isobaricInhPa': 'levels'})
+
+        # projection information
+        ds_out["proj"] = int()
+        for k, v in CRS.from_user_input(ds.herbie.crs).to_cf().items():
+            ds_out.proj.attrs[k] = v
+        for var in ds_out.data_vars:
+            ds_out[var].attrs['grid_mapping'] = 'proj'
+
+        # subset by AOI
+        x_min, x_max, y_min, y_max = get_bounds_indices(
+            self._ll_bounds, 
+            ds_out.latitude.to_numpy(), 
+            ds_out.longitude.to_numpy(),
+        )
+        ds_out = ds_out.sel(x=slice(x_min, x_max), y=slice(y_min, y_max))
+        
+        ds_out.to_netcdf(out, engine='netcdf4')
+
+        return
+
+
+def get_bounds_indices(SNWE, lats, lons):
+    '''
+    Convert SNWE lat/lon bounds to index bounds
+    '''
+    # Unpack the bounds and find the relevent indices 
+    S, N, W, E = SNWE
+    m1 = (S <= lats) & (N >= lats) &\
+                (W <= lons) & (E >= lons)
+
+    if np.sum(m1) == 0:
+        raise RuntimeError('Area of Interest has no overlap with the HRRR model available extent')
+
+    # Y extent
+    shp = lats.shape
+    m1_y = np.argwhere(np.sum(m1, axis=1) != 0)
+    y_min = max(m1_y[0][0] - 2, 0)
+    y_max = min(m1_y[-1][0] + 3, shp[0])
+    m1_y = None
+
+    # X extent
+    m1_x = np.argwhere(np.sum(m1, axis=0) != 0)
+    x_min = max(m1_x[0][0] - 2, 0)
+    x_max = min(m1_x[-1][0] + 3, shp[1])
+    m1_x = None
+    m1 = None
+
+    return x_min, x_max, y_min, y_max
