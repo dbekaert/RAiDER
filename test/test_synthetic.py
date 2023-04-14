c5c67ea5927f68392e9dedd8f11a5a9b2ac5fc67
bbuzz31 <bbuzz318@icloud.com>
2023-04-14 14:26:41 -0400

    Update docs, cosmetics

--- test/test_synthetic.py
+++ test/test_synthetic.py
@@ -44,9 +44,17 @@ def update_yaml(dct_cfg:dict, dst:str='temp.yaml'):
     return dst
 
 
-def update_model(wm_file:str, kind:str, wm_dir:str='weather_files_synth'):
-    """ Update the weater model file; keep kind to equation to test """
-    assert kind in 'hydro wet1 wet2', 'Set  kind to hydro, wet1, or wet2'
+def update_model(wm_file:str, wm_eq_type:str, wm_dir:str='weather_files_synth'):
+    """ Update weather model file by the equation to test, write it to disk
+
+    wm_eq_type is one of: [hydro, wet_linear, wet_nonlinear]
+    Hydro Refractivity = k1 * (Pressure/Temp), set Pressure = Temp
+    Wet Refractivity = k2 * (RelHumidty/Temp) + k3 * (RelHumidity/Temp^2)
+        for linear component, set RelHumidty = Temp, k3 = 0
+        for nonlinear component, set RelHumidty = Temp^2, k2 = 0
+    """
+    assert wm_eq_type in 'hydro wet_linear wet_nonlinear', \
+                    'Set  wm_eq_type to hydro, wet_linear, or wet_nonlinear'
     from RAiDER.models.gmao import GMAO
     # initialize dummy wm to calculate constant delays
     # any model will do as 1) all constants same 2) all equations same
@@ -56,12 +64,12 @@ def update_model(wm_file:str, kind:str, wm_dir:str='weather_files_synth'):
     t  = ds['t']
     p  = ds['p']
     e  = ds['e']
-    if kind == 'hydro':
+    if wm_eq_type == 'hydro':
         p = t
-    elif kind == 'wet1':
+    elif wm_eq_type == 'wet_linear':
         e = t
         Obj._k3 = 0
-    elif kind == 'wet2':
+    elif wm_eq_type == 'wet_nonlinear':
         e = t**2
         Obj._k2 = 0
 
@@ -151,9 +159,15 @@ def length_of_ray(target_xyz:list, model_zs, los):
 
 @dataclass
 class StudyArea(object):
-    """ Object with shared parameters related to the study area """
-    reg:str
-    WM:str
+    """ Object with shared parameters related to the study area
+
+    region the short name corresponding to a specific bounding box.
+    Choose from:
+        LA (Los Angeles, California; mid latitude)
+        Fort (Fortaleza, Brazil; equator)
+    """
+    region:str
+    wmName:str
     wd = op.join(TEST_DIR, 'synthetic_test')
 
     def __post_init__(self):
@@ -162,7 +176,7 @@ class StudyArea(object):
         self.dts   = self.dt.strftime('%Y_%m_%d_T%H_%M_%S')
         self.ttime = self.dt.strftime('%H:%M:%S')
 
-        self.wmObj = modelName2Module(self.WM.upper().replace("-", ""))[1]()
+        self.wmObj = modelName2Module(self.wmName.upper().replace("-", ""))[1]()
 
         aoi = BoundingBox(self.SNWE)
         aoi.add_buffer(buffer = 1.5 * self.wmObj.getLLRes())
@@ -171,7 +185,7 @@ class StudyArea(object):
 
         wm_bounds = aoi.calc_buffer_ray(self.los.getSensorDirection(), lookDir=self.los.getLookDirection())
         self.wmObj.set_latlon_bounds(wm_bounds)
-        wm_fname  = make_weather_model_filename(self.WM, self.dt, self.wmObj._ll_bounds)
+        wm_fname  = make_weather_model_filename(self.wmName, self.dt, self.wmObj._ll_bounds)
         self.path_wm_real = op.join(self.wd, 'weather_files_real', wm_fname)
 
         grid_spacing = 0.5
@@ -180,13 +194,21 @@ class StudyArea(object):
 
 
     def setup_region(self):
-        if self.reg == 'LA':
+        """ Setup the bounding box and choose orbit file based on region name
+
+        Possible regions are:
+            LA (Los Angeles, California; midlatitude)
+            Fort (Fortaleza, Brazil; equator)
+        """
+        # Los Angeles
+        if self.region == 'LA':
             self.SNWE  = 33, 34, -118.25, -117.25
             self.dt    = datetime(2020, 1, 30, 13, 52, 45)
             self.orbit = self.wd + \
                 '/S1B_OPER_AUX_POEORB_OPOD_20210317T025713_V20200129T225942_20200131T005942.EOF'
 
-        elif self.reg == 'Fort':
+        # Fortaleza, Brazil
+        elif self.region == 'Fort':
             self.SNWE = -4.0, -3.5, -38.75, -38.25
             self.dt   = datetime(2019, 11, 17, 20, 51, 58)
             self.orbit = self.wd + \
@@ -201,18 +223,18 @@ class StudyArea(object):
             'date_group': {'date_list': datetime.strftime(self.dt, '%Y%m%d')},
             'cube_spacing_in_m': str(self.cube_spacing),
             'los_group': {'ray_trace': True, 'orbit_file': self.orbit},
-            'weather_model': self.WM,
+            'weather_model': self.wmName,
             'runtime_group': {'output_directory': self.wd},
         }
         return dct
 
 
-def dl_real(reg='LA', mod='GMAO'):
+def dl_real(region='LA', mod='GMAO'):
     """ Download the real weather model to overwrite
 
     This 'golden dataset' shouldnt be changed
     """
-    SAobj = StudyArea(reg, mod)
+    SAobj = StudyArea(region, mod)
     dct_cfg = SAobj.make_config_dict()
     # set the real weather model path and download only
     dct_cfg['runtime_group']['weather_model_directory'] = \
@@ -225,24 +247,28 @@ def dl_real(reg='LA', mod='GMAO'):
     cmd  = f'raider.py {cfg}'
 
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
-    return
+    assert proc.returncode == 0, 'RAiDER did not complete successfully'
 
 
-def test_hydrostatic_eq(reg='Fort', mod='GMAO'):
-    """ Test the hydrostatic eqn by setting t=p and comparing to ray
+def test_hydrostatic_eq(region='Fort', mod='GMAO'):
+    """ Test hydrostatic equation: Hydro Refractivity = k1 * (Pressure/Temp)
 
-    The ray length is computed here (length_of_ray; m), and in raider (m * K/Pa)
-    ray length computed here is then scaled by constant K/Pa
-    ray length from raider is unscaled from parts per million
-    We check they are both large enough for meaningful numerical comparison (>1)
-    We then compute residual and normalize it we theoretical ray length
-    We ensure that normalized residual is not significantly different from 0
+    The hydrostatic delay reduces to an integral along the ray path when P=T.
+    However the constants k1 and scaling of 10^-6 will remain present leading
+    to a result of ray-length * k1 * 10^-6. We specifically do the following:
+
+    Compute ray length here (length_of_ray; m).
+    Run raider with P=T to return delays in m * K/Pa
+    Scale ray length computed here by constant (K/Pa)
+    (Un)scale raider delay by parts per million term (*1e6)
+    Check they are both large enough for meaningful numerical comparison (>1)
+    Compute residual and normalize by theoretical ray length (calculated here)
+    Ensure that normalized residual is not significantly different from 0
         significantly different = 7 decimal places
     """
 
     ## setup the config files
-    SAobj = StudyArea(reg, mod)
+    SAobj = StudyArea(region, mod)
     dct_cfg      = SAobj.make_config_dict()
     wm_dir_synth = op.dirname(SAobj.path_wm_real).replace('real', 'synth')
     dct_cfg['runtime_group']['weather_model_directory'] = wm_dir_synth
@@ -256,11 +282,11 @@ def test_hydrostatic_eq(reg='Fort', mod='GMAO'):
     ## run raider with the synthetic model
     cmd  = f'raider.py {cfg}'
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
+    assert proc.returncode == 0, 'RAiDER did not complete successfully'
 
     # get the just created synthetic delays
     ds = xr.open_dataset(
-        f'{SAobj.wd}/{SAobj.WM}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
+        f'{SAobj.wd}/{SAobj.wmName}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
     da = ds['hydro']
     ds.close()
     del ds
@@ -285,41 +311,48 @@ def test_hydrostatic_eq(reg='Fort', mod='GMAO'):
 
     da.close()
     del da
-    return
 
 
-def test_wet_eq1(reg='Fort', mod='GMAO'):
-    """ Test the wet eqn (k2 part) by setting t=e and comparing to ray
+def test_wet_eq_linear(region='Fort', mod='GMAO'):
+    """ Test linear part of wet equation.
 
-    The ray length is computed here (length_of_ray; m), and in raider (m * K/Pa)
-    ray length computed here is then scaled by constant K/Pa
-    ray length from raider is unscaled from parts per million
-    We check they are both large enough for meaningful numerical comparison (>1)
-    We then compute residual and normalize it we theoretical ray length
-    We ensure that normalized residual is not significantly different from 0
+    Wet Refractivity = k2 * (E/T) + k3 * (E/T^2)
+    E = relative humidty; T = temperature
+
+    The wet delay reduces to an integral along the ray path when E=T and k3 = 0
+    However the constants k2 and scaling of 10^-6 will remain present, leading
+    to a result of ray-lengh * k2 * 10^-6. We specifically do the following:
+
+    Computed ray length here (length_of_ray; m).
+    Run raider with E=T and k3=0 to return delays in m * K/Pa
+    Scale ray length computed here by constant (K/Pa)
+    (Un)scale raider delay by parts per million term (*1e6)
+    Check they are both large enough for meaningful numerical comparison (>1)
+    Compute residual and normalize by theoretical ray length (calculated here)
+    Ensure that normalized residual is not significantly different from 0
         significantly different = 7 decimal places
     """
 
     ## setup the config files
-    SAobj = StudyArea(reg, mod)
+    SAobj = StudyArea(region, mod)
     dct_cfg      = SAobj.make_config_dict()
     wm_dir_synth = op.dirname(SAobj.path_wm_real).replace('real', 'synth')
     dct_cfg['runtime_group']['weather_model_directory'] = wm_dir_synth
     dct_cfg['download_only'] = False
 
     ## update the weather model; t = e for wet1
-    path_synth = update_model(SAobj.path_wm_real, 'wet1', wm_dir_synth)
+    path_synth = update_model(SAobj.path_wm_real, 'wet_linear', wm_dir_synth)
 
     cfg = update_yaml(dct_cfg)
 
     ## run raider with the synthetic model
     cmd  = f'raider.py {cfg}'
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
+    assert proc.returncode == 0, 'RAiDER did not complete successfully'
 
     # get the just created synthetic delays
     ds = xr.open_dataset(
-        f'{SAobj.wd}/{SAobj.WM}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
+        f'{SAobj.wd}/{SAobj.wmName}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
     da = ds['wet']
     ds.close()
     del ds
@@ -344,40 +377,47 @@ def test_wet_eq1(reg='Fort', mod='GMAO'):
 
     da.close()
     del da
-    return
 
 
-def test_wet_eq2(reg='Fort', mod='GMAO'):
-    """ Test the wet eqn (k2 part) by setting t=e and comparing to ray
+def test_wet_eq_nonlinear(region='Fort', mod='GMAO'):
+    """ Test the nonlinear part of the wet equation.
 
-    The ray length is computed here (length_of_ray; m), and in raider (m * K^2/Pa)
+    Wet Refractivity = k2 * (E/T) + k3 * (E/T^2)
+    E = relative humidty; T = temperature
+
+    The wet delay reduces to an integral along the ray path when E=T^2 and k2=0
+    However the constants k3 and scaling of 10^-6 will remain present leading
+    to a result of ray-lengh * k3 * 10^-6. We specifically do the following:
+
+    Computed ray length here (length_of_ray; m), and run raider with P=T (m * K^2/Pa)
     ray length computed here is then scaled by constant K^2/Pa
-    ray length from raider is unscaled from parts per million
+    delay from raider is unscaled from parts per million (*1e6)
     We check they are both large enough for meaningful numerical comparison (>1)
-    We then compute residual and normalize it we theoretical ray length
+    We then compute residual and normalize it with theoretical ray length
     We ensure that normalized residual is not significantly different from 0
         significantly different = 7 decimal places
     """
+
     ## setup the config files
-    SAobj = StudyArea(reg, mod)
+    SAobj = StudyArea(region, mod)
     dct_cfg      = SAobj.make_config_dict()
     wm_dir_synth = op.dirname(SAobj.path_wm_real).replace('real', 'synth')
     dct_cfg['runtime_group']['weather_model_directory'] = wm_dir_synth
     dct_cfg['download_only'] = False
 
     ## update the weather model; t = e for wet1
-    path_synth = update_model(SAobj.path_wm_real, 'wet2', wm_dir_synth)
+    path_synth = update_model(SAobj.path_wm_real, 'wet_nonlinear', wm_dir_synth)
 
     cfg = update_yaml(dct_cfg)
 
     ## run raider with the synthetic model
     cmd  = f'raider.py {cfg}'
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
+    assert proc.returncode == 0, 'RAiDER did not complete successfully'
 
     # get the just created synthetic delays
     ds = xr.open_dataset(
-        f'{SAobj.wd}/{SAobj.WM}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
+        f'{SAobj.wd}/{SAobj.wmName}_tropo_{SAobj.dts.replace("_", "")}_ray.nc')
     da = ds['wet']
     ds.close()
     del ds
@@ -404,4 +444,3 @@ def test_wet_eq2(reg='Fort', mod='GMAO'):
     os.remove('./error.log')
     os.remove('./debug.log')
     del da
-    return
