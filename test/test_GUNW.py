588f8a502dde81f3afa62895d167f766e487e116
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-04-19 16:28:29 -0700

    GUNW test and clean up

--- test/test_GUNW.py
+++ test/test_GUNW.py
@@ -1,53 +1,40 @@
 import glob
+import json
 import os
 import shutil
 import subprocess
 
 import numpy as np
+import pytest
+import jsonschema
 import rasterio as rio
 import xarray as xr
-import pytest
 
-from test import TEST_DIR
+from RAiDER import aws
+from RAiDER.cli.raider import calcDelaysGUNW
+import RAiDER.cli.raider as raider
 
-WM = 'GMAO'
+
+WEATHER_MODEL_NAME = 'GMAO'
+
 
 @pytest.mark.isce3
-def test_GUNW():
-    ## eventually to be implemented
-    # home = os.path.expanduser('~')
-    # netrc = os.path.join(home, '.netrc')
-#
-    # ## make netrc
-    # if not os.path.exists(netrc):
-        # name, passw = os.getenv('URSname'), os.getenv('URSpass')
-        # cmd = f'echo "machine urs.earthdata.nasa.gov login {name} password {passw}" > ~/.netrc'
-        # subprocess.run(cmd.split())
-#
-        # cmd = f'chmod 600 {netrc}'
-        # subprocess.run(cmd.split())
-#
-    SCENARIO_DIR = os.path.join(TEST_DIR, "GUNW")
-    os.makedirs(SCENARIO_DIR, exist_ok=True)
-    GUNW = 'S1-GUNW-D-R-071-tops-20200130_20200124-135156-34956N_32979N-PP-913f-v2_0_4.nc'
-    orig_GUNW = os.path.join(TEST_DIR, GUNW)
-    updated_GUNW = os.path.join(SCENARIO_DIR, GUNW)
+def test_GUNW_update(test_dir_path, test_gunw_path):
+    scenario_dir = test_dir_path / 'GUNW'
+    scenario_dir.mkdir(exist_ok=True, parents=True)
+    orig_GUNW = test_gunw_path
+    updated_GUNW = scenario_dir / orig_GUNW.name
     shutil.copy(orig_GUNW, updated_GUNW)
 
-
-    # cmd  = f'wget https://grfn.asf.alaska.edu/door/download/{GUNW}'
-    # proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    # assert np.isclose(proc.returncode, 0)
-
-    cmd  = f'raider.py ++process calcDelaysGUNW -f {updated_GUNW} -m {WM} -o {SCENARIO_DIR}'
+    cmd = f'raider.py ++process calcDelaysGUNW -f {updated_GUNW} -m {WEATHER_MODEL_NAME} -o {scenario_dir}'
     proc = subprocess.run(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    assert np.isclose(proc.returncode, 0)
+    assert proc.returncode == 0
 
-    ## check the CRS and affine are written correctly
-    epsg      = 4326
+    # check the CRS and affine are written correctly
+    epsg = 4326
     transform = (0.1, 0.0, -119.85, 0, -0.1, 35.55)
 
-    group = f'science/grids/corrections/external/troposphere/{WM}/reference'
+    group = f'science/grids/corrections/external/troposphere/{WEATHER_MODEL_NAME}/reference'
     for v in 'troposphereWet troposphereHydrostatic'.split():
         ds = rio.open(f'netcdf:{updated_GUNW}:{group}/{v}')
         with rio.open(f'netcdf:{updated_GUNW}:{group}/{v}') as ds:
@@ -63,9 +50,42 @@ def test_GUNW():
         crs = rio.crs.CRS.from_wkt(ds['crs'].crs_wkt)
         assert np.isclose(crs.to_epsg(), epsg), 'CRS incorrect'
 
-
     # Clean up files
-    shutil.rmtree(SCENARIO_DIR)
+    shutil.rmtree(scenario_dir)
     os.remove('GUNW_20200130-20200124_135156.yaml')
-    [os.remove(f) for f in glob.glob(f'{WM}*')]
+    [os.remove(f) for f in glob.glob(f'{WEATHER_MODEL_NAME}*')]
     return
+
+
+def test_GUNW_metadata_update(test_gunw_json_path, test_gunw_json_schema_path, monkeypatch):
+    temp_json_path = str(test_gunw_json_path)
+    temp_json_path = temp_json_path.replace('.json', '-temp.json')
+    shutil.copy(test_gunw_json_path, temp_json_path)
+    iargs = ['--weather-model', 'HRES',
+             '--bucket', 'foo',
+             '--bucket-prefix', 'bar']
+
+    def do_nothing_factory(length_of_return_list: int = 0):
+        n = length_of_return_list
+        items = ['foo'] * n if n else None
+
+        def do_nothing(*args, **kwargs) -> list:
+            return items
+        return do_nothing
+
+    def mock_s3_file(*args):
+        return str(temp_json_path)
+
+    monkeypatch.setattr(aws, "get_s3_file", mock_s3_file)
+    monkeypatch.setattr(aws, "upload_file_to_s3", do_nothing_factory())
+    monkeypatch.setattr(raider, "GUNW_prep", do_nothing_factory(2))
+    monkeypatch.setattr(raider, "calcDelays", do_nothing_factory(2))
+    monkeypatch.setattr(raider, "GUNW_calc", do_nothing_factory())
+
+    calcDelaysGUNW(iargs)
+
+    metadata = json.load(open(temp_json_path))
+    schema = json.load(open(test_gunw_json_schema_path))
+
+    assert metadata['weather_model'] == ['HRES']
+    assert (jsonschema.validate(instance=metadata, schema=schema) is None)
