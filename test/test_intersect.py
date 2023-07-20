aabc3b166d8c9bb741a7ee0f75221bc3637b63bc
Brett Buzzanga <bbuzz318@icloud.com>
2023-07-19 21:20:47 -0700

    use smaller dem for circleci

--- test/test_intersect.py
+++ test/test_intersect.py
@@ -14,10 +14,10 @@ def test_cube_intersect(wm):
     os.makedirs(SCENARIO_DIR, exist_ok=True)
 
     ## make the lat lon grid
-    S, N, W, E = 33, 34.5, -118.0, -116.5
+    S, N, W, E = 33.5, 34, -118.0, -117.5
     date       = 20200130
     time       ='13:52:45'
-    f_lat, f_lon = makeLatLonGrid([S, N, W, E], 'LA', SCENARIO_DIR, 0.5)
+    f_lat, f_lon = makeLatLonGrid([S, N, W, E], 'LA', SCENARIO_DIR, 0.25)
 
     ## make the template file
     grp = {
@@ -39,12 +39,12 @@ def test_cube_intersect(wm):
     assert proc.returncode == 0, 'RAiDER Failed.'
 
     ## hard code what it should be and check it matches
-    gold = {'ERA5': 2.290632486, 'GMAO': np.nan, 'HRRR': np.nan}
+    gold = {'ERA5': 2.29017997, 'GMAO': np.nan, 'HRRR': np.nan}
 
     path_delays = os.path.join(SCENARIO_DIR, f'{wm}_hydro_{date}T{time.replace(":", "")}_ztd.tiff')
     da  = xrr.open_rasterio(path_delays, band_as_variable=True)['band_1']
     hyd = da.sel(x=-117.8, y=33.4, method='nearest').item()
-    np.testing.assert_almost_equal(gold[wm], hyd)
+    np.testing.assert_almost_equal(hyd, gold[wm])
 
     # Clean up files
     shutil.rmtree(SCENARIO_DIR)
