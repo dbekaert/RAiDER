01ea3b57dbce56949bb9a46f95293ea940bdd43b
Brett Buzzanga <bbuzz318@icloud.com>
2023-08-17 15:19:34 -0700

    Get rid of my debugging statements

--- tools/RAiDER/delay.py
+++ tools/RAiDER/delay.py
@@ -314,18 +314,6 @@ def _build_cube_ray(
                 # For each interpolator, integrate between levels
                 for mm, out in enumerate(outSubs):
                     val =  interpolators[mm](pts)
-                    if np.isnan(val).any() and ht == 3500:
-                        ray_lengths, low_xyzs, high_xyzs = \
-                            build_ray(model_zs, ht, xyz, LOS,  MAX_TROPO_HEIGHT, bp=True)
-
-                    elif np.isnan(val).any() and ht == 4000:
-                        ray_lengths, low_xyzs, high_xyzs = \
-                            build_ray(model_zs, ht, xyz, LOS,  MAX_TROPO_HEIGHT, bp=True)
-
-                    # if ht == 9000 and zz == 82:
-                    #     ray_lengths, low_xyzs, high_xyzs = \
-                    #         build_ray(model_zs, ht, xyz, LOS,  MAX_TROPO_HEIGHT, bp=True)
-
 
                     # TODO - This should not occur if there is enough padding in model
                     # val[np.isnan(val)] = 0.0
