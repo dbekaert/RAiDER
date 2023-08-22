78b85034d6fe94e88ec0ba895ff7dd8e8b5d4ced
Jeremy Maurer <maurer.jeremy@gmail.com>
2023-08-22 16:16:38 -0500

    Update constants.py to include zref comment
    Adds note that zref is used when not otherwise specified by the user

--- tools/RAiDER/constants.py
+++ tools/RAiDER/constants.py
@@ -8,7 +8,7 @@
 import numpy as np
 
 _ZMIN = np.float64(-100)   # minimum required height
-_ZREF = np.float64(26000)  # maximum integration height
+_ZREF = np.float64(26000)  # maximum integration height when not specified by user
 _STEP = np.float64(15.0)   # integration step size in meters
 
 _g0 = np.float64(9.80665) # Standard gravitational constant
