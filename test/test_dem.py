305f584dd0c1c7faa3b9f0838a9f03ebff43bde4
Jeremy Maurer <jmaurer@mst.edu>
2023-10-03 07:33:37 -0500

    fix a small bug in the dem unit test

--- test/test_dem.py
+++ test/test_dem.py
@@ -8,7 +8,10 @@ from RAiDER.dem import download_dem
 
 def test_download_dem_1():
     SCENARIO_1 = os.path.join(TEST_DIR, "scenario_1")
-    hts = download_dem(outName=os.path.join(SCENARIO_1,'geom', 'hgt.rdr'), overwrite=False)
+    hts = download_dem(
+        outName=os.path.join(SCENARIO_1,'geom', 'hgt.rdr'), 
+        overwrite=False
+    )
     assert True
 
 
