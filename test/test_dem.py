c65b2627d75eaaac31ed28cb74293bebd92a4bb7
Jeremy Maurer <jmaurer@mst.edu>
2023-10-03 10:11:32 -0500

    update dem unit test

--- test/test_dem.py
+++ test/test_dem.py
@@ -7,11 +7,12 @@ from RAiDER.dem import download_dem
 
 
 def test_download_dem_1():
-    SCENARIO_1 = os.path.join(TEST_DIR, "scenario_1")
-    hts = download_dem(
-        outName=os.path.join(SCENARIO_1,'geom', 'hgt.rdr'), 
+    SCENARIO_1 = os.path.join(TEST_DIR, "scenario_4")
+    hts, meta = download_dem(
+        outName=os.path.join(SCENARIO_1,'warpedDEM.dem'), 
         overwrite=False
     )
-    assert True
+    assert hts.shape == (45,226)
+    assert meta['crs'] is None
 
 
