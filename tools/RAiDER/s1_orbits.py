46450497ad358732c9b1c9e7720c9308b2da662a
Brett Buzzanga <bbuzz318@icloud.com>
2024-04-10 18:30:20 -0700

    get orbits individually

--- tools/RAiDER/s1_orbits.py
+++ tools/RAiDER/s1_orbits.py
@@ -58,6 +58,8 @@ def get_orbits_from_slc_ids(slc_ids: List[str], directory=Path.cwd()) -> List[Pa
     missions = [slc_id[0:3] for slc_id in slc_ids]
     start_times = [re.split(r'_+', slc_id)[4] for slc_id in slc_ids]
     stop_times = [re.split(r'_+', slc_id)[5] for slc_id in slc_ids]
+    # logger.warning('st+stop %s', start_times+stop_times)
+    # logger.warning('missions %s', missions)
 
     orb_files = download_eofs(start_times + stop_times, missions * 2, str(directory))
 
@@ -67,10 +69,19 @@ def get_orbits_from_slc_ids(slc_ids: List[str], directory=Path.cwd()) -> List[Pa
 def download_eofs(dts:list, missions:list, save_dir:str):
     """ Wrapper to first try downloading from ASF """
     import eof.download
-    try:
-        orb_files = eof.download.download_eofs(dts, missions, save_dir=save_dir, force_asf=True)
-    except:
-        logger.warning('Could not download orbit from ASF, trying ESA...')
-        orb_files = eof.download.download_eofs(dts, missions, save_dir=save_dir, force_asf=False)
+    orb_files = []
+    for dt, mission in zip(dts, missions):
+        dt = dt if isinstance(dt, list) else [dt]
+        mission = mission if isinstance(mission, list) else [mission]
+        try:
+            orb_file = eof.download.download_eofs(dt, mission, save_dir=save_dir, force_asf=True)
+            orb_files.append(orb_file)
+        except:
+            logger.error(f'Could not download orbit from ASF, trying ESA...')
+            orb_file = eof.download.download_eofs(dt, mission, save_dir=save_dir, force_asf=False)
+            orb_files.append(orb_file)
+
+    if not len(orb_files) == len(dts):
+        raise Exception(f'Missing {len(dts) - len(orb_files)} orbit files! dts={dts}, orb_files={len(orb_files)}')
 
     return orb_files
