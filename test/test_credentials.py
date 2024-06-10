124a00700a58badf9e7e0ed6595472fee80b4195
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-06-10 13:16:51 -0500

    autopep8

--- test/test_credentials.py
+++ test/test_credentials.py
@@ -3,23 +3,26 @@ from platform import system
 from RAiDER.models import credentials
 
 # Test checking/creating ECMWF_RC CAPI file
+
+
 def test_ecmwfApi_createFile():
     import ecmwfapi
 
-    #Check extension for hidden files
-    hidden_ext = '_' if system()=="Windows" else '.'
+    # Check extension for hidden files
+    hidden_ext = '_' if system() == "Windows" else '.'
 
     # Test creation of ~/.ecmwfapirc file
-    ecmwf_file = os.path.expanduser('./') + hidden_ext + credentials.API_FILENAME['HRES']
+    ecmwf_file = os.path.expanduser(
+        './') + hidden_ext + credentials.API_FILENAME['HRES']
     credentials.check_api('HRES', 'dummy', 'dummy', './', update_flag=True)
-    assert os.path.exists(ecmwf_file) == True,f'{ecmwf_file} does not exist'
+    assert os.path.exists(ecmwf_file) == True, f'{ecmwf_file} does not exist'
 
     # Get existing ECMWF_API_RC env if exist
     default_ecmwf_file = os.getenv("ECMWF_API_RC_FILE")
     if default_ecmwf_file is None:
         default_ecmwf_file = ecmwfapi.api.DEFAULT_RCFILE_PATH
 
-    #Set it to current dir to avoid overwriting ~/.ecmwfapirc file
+    # Set it to current dir to avoid overwriting ~/.ecmwfapirc file
     os.environ["ECMWF_API_RC_FILE"] = ecmwf_file
     key, url, uid = ecmwfapi.api.get_apikey_values()
 
@@ -27,8 +30,8 @@ def test_ecmwfApi_createFile():
     os.environ["ECMWF_API_RC_FILE"] = default_ecmwf_file
     os.remove(ecmwf_file)
 
-    #Check if API is written correctly
-    assert uid ==  'dummy', f'{ecmwf_file}: UID was not written correctly'
+    # Check if API is written correctly
+    assert uid == 'dummy', f'{ecmwf_file}: UID was not written correctly'
     assert key == 'dummy', f'{ecmwf_file}: KEY was not written correctly'
 
 
@@ -37,13 +40,14 @@ def test_ecmwfApi_createFile():
 def test_cdsApi_createFile():
     import cdsapi
 
-    #Check extension for hidden files
-    hidden_ext = '_' if system()=="Windows" else '.'
+    # Check extension for hidden files
+    hidden_ext = '_' if system() == "Windows" else '.'
 
     # Test creation of .cdsapirc file in current dir
-    cds_file = os.path.expanduser('./') + hidden_ext + credentials.API_FILENAME['ERA5']
+    cds_file = os.path.expanduser(
+        './') + hidden_ext + credentials.API_FILENAME['ERA5']
     credentials.check_api('ERA5', 'dummy', 'dummy', './', update_flag=True)
-    assert os.path.exists(cds_file) == True,f'{cds_file} does not exist'
+    assert os.path.exists(cds_file), f'{cds_file} does not exist'
 
     # Check the content
     cds_credentials = cdsapi.api.read_config(cds_file)
@@ -52,20 +56,21 @@ def test_cdsApi_createFile():
     # Remove local API file
     os.remove(cds_file)
 
-    assert uid ==  'dummy', f'{cds_file}: UID was not written correctly'
+    assert uid == 'dummy', f'{cds_file}: UID was not written correctly'
     assert key == 'dummy', f'{cds_file}: KEY was not written correctly'
 
 # Test checking/creating EARTHDATA_RC API file
 def test_netrcApi_createFile():
     import netrc
 
-    #Check extension for hidden files
-    hidden_ext = '_' if system()=="Windows" else '.'
+    # Check extension for hidden files
+    hidden_ext = '_' if system() == "Windows" else '.'
 
     # Test creation of ~/.cdsapirc file
-    netrc_file = os.path.expanduser('./') + hidden_ext + credentials.API_FILENAME['GMAO']
+    netrc_file = os.path.expanduser(
+        './') + hidden_ext + credentials.API_FILENAME['GMAO']
     credentials.check_api('GMAO', 'dummy', 'dummy', './', update_flag=True)
-    assert os.path.exists(netrc_file) == True,f'{netrc_file} does not exist'
+    assert os.path.exists(netrc_file) == True, f'{netrc_file} does not exist'
 
     # Check the content
     host = 'urs.earthdata.nasa.gov'
@@ -75,5 +80,5 @@ def test_netrcApi_createFile():
     # Remove local API file
     os.remove(netrc_file)
 
-    assert uid ==  'dummy', f'{netrc_file}: UID was not written correctly'
+    assert uid == 'dummy', f'{netrc_file}: UID was not written correctly'
     assert key == 'dummy', f'{netrc_file}: KEY was not written correctly'
