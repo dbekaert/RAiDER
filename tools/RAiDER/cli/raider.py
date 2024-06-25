f3aa4bdac2624e2417538aea286c4e0654385de7
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-06-25 16:58:20 -0500

    Integrate example run configurations into CLI
    - --generate_config now takes an argument: the name of one example run configuration of the now four that exist
    - argparse logic fixed to make --generate_config mutually exclusive with passing in a run config file; i.e., you can't both generate a config and execute RAiDER with a config file at once
    - --generate_config now confirms before overwriting files
    - Logic added to allow for example run configs to have data files, like two of the three raiderDelay.py example commands did. These data files will be copied to the current directory next to the run config file itself

--- tools/RAiDER/cli/raider.py
+++ tools/RAiDER/cli/raider.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import glob
 import os
 import json
 import shutil
@@ -10,11 +11,11 @@ import numpy as np
 import xarray as xr
 
 from textwrap import dedent
+from pathlib import Path
 
 import RAiDER.aria.prepFromGUNW
 import RAiDER.aria.calcGUNW
 from RAiDER import aws
-from RAiDER.constants import _ZREF
 from RAiDER.logger import logger, logging
 from RAiDER.cli import DEFAULT_DICT, AttributeDict
 from RAiDER.cli.parser import add_out, add_cpus, add_verbose
@@ -24,8 +25,6 @@ from RAiDER.models.customExceptions import *
 from RAiDER.utilFcns import get_dt
 from RAiDER.s1_azimuth_timing import get_s1_azimuth_time_grid, get_inverse_weights_for_dates, get_times_for_azimuth_interpolation
 
-
-import traceback
 
 TIME_INTERPOLATION_METHODS = ['none', 'center_time', 'azimuth_time_grid']
 
@@ -37,16 +36,13 @@ raider.py --generate_config
 Download a weather model and calculate tropospheric delays
 """
 
-SHORT_MESSAGE = """
-Program to calculate troposphere total delays using a weather model
-"""
-
 EXAMPLES = """
 Usage examples:
 raider.py -g
 raider.py run_config_file.yaml
 """
 
+DEFAULT_RUN_CONFIG_PATH = os.path.abspath("./raider.yaml")
 
 def read_run_config_file(fname):
     """
@@ -146,70 +142,83 @@ def calcDelays(iargs=None):
     from RAiDER.checkArgs import checkArgs
     from RAiDER.utilFcns import writeDelays, get_nearest_wmtimes
     examples = 'Examples of use:' \
-        '\n\t raider.py customTemplatefile.cfg' \
-        '\n\t raider.py -g'
+        '\n\t raider.py run_config_file.yaml' \
+        '\n\t raider.py --generate_config template'
 
     p = argparse.ArgumentParser(
-        description =
-            'Command line interface for RAiDER processing with a configure file.'
-            'Default options can be found by running: raider.py --generate_config',
+        description = HELP_MESSAGE,
         epilog=examples, formatter_class=argparse.RawDescriptionHelpFormatter)
 
     p.add_argument(
-        'customTemplateFile', nargs='?',
-        help='custom template with option settings.\n' +
-        "ignored if the default smallbaselineApp.cfg is input."
-    )
-
-    p.add_argument(
-        '-g', '--generate_template', action='store_true',
-        help='generate default template (if it does not exist) and exit.'
-    )
-
-    p.add_argument(
-        '--download_only', action='store_true',
+        '--download_only',
+        action='store_true',
         help='only download a weather model.'
     )
 
-    ## if not None, will replace first argument (run_config_file)
+    # Generate an example configuration file, OR
+    # run with a configuration file.
+    # TODO(garlic-os): run `./raider.yaml` when neither given.
+    #                  required=False?
+    group = p.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        '--generate_config', '-g',
+        nargs='?',
+        choices=[
+            'template',
+            'example_LA_bbox',
+            'example_LA_GNSS',
+            'example_UK_isce',
+        ],
+        help='Generate an example run configuration and exit'
+    )
+    group.add_argument(
+        'run_config_file',
+        nargs='?',
+        help='a YAML file with arguments to RAiDER'
+    )
+
+    # if not None, will replace first argument (run_config_file)
+    # TODO(garlic-os): what is this
     args = p.parse_args(args=iargs)
 
-    # default input file
-    template_file = os.path.join(os.path.dirname(RAiDER.__file__),
-                                                        'cli', 'raider.yaml')
+    # Default example run configuration file
+    ex_run_config_name = args.generate_config or 'template'
+    ex_run_config_dir = (
+        Path(RAiDER.__file__).parent /
+        'cli/examples' / ex_run_config_name
+    )
 
-    if args.generate_template:
-        dst = os.path.join(os.getcwd(), 'raider.yaml')
-        shutil.copyfile(example_run_config_path, dst)
-        logger.info('Wrote: %s', dst)
+    if args.generate_config is not None:
+        for filename in ex_run_config_dir.glob('*'):
+            dest_path = Path(os.getcwd()) / filename.name
+            if dest_path.exists():
+                print(f'File {dest_path} already exists. Overwrite? [y/n]')
+                if input().lower() != 'y':
+                    continue
+            shutil.copy(filename, os.getcwd())
+            logger.info('Wrote: %s', filename)
         sys.exit()
+    # args.generate_config now guaranteed to be None
 
-
-    # check: existence of input template files
-    if (not args.customTemplateFile
-            and not os.path.isfile(os.path.basename(template_file))
-            and not args.generate_template):
-        msg = "No template file found! It requires that either:"
-        msg += "\n  a custom template file, OR the default template "
-        msg += "\n  file 'raider.yaml' exists in current directory."
-
-        p.print_usage()
-        print(examples)
-        raise SystemExit(f'ERROR: {msg}')
-
-    if  args.run_config_file:
-        # check the existence
+    # If no run configuration file is provided, look for a ./raider.yaml
+    if args.run_config_file is not None:
         if not os.path.isfile(args.run_config_file):
             raise FileNotFoundError(args.run_config_file)
-
-        args.run_config_file = os.path.abspath(args.run_config_file)
     else:
-        args.run_config_file = example_run_config_path
+        if not os.path.isfile(DEFAULT_RUN_CONFIG_PATH):
+            msg = (
+                "No run configuration file provided! Specify a run configuration "
+                "file or have a 'raider.yaml' file in the current directory."
+            )
+            p.print_usage()
+            print(examples)
+            raise SystemExit(f'ERROR: {msg}')
+        args.run_config_file = DEFAULT_RUN_CONFIG_PATH
 
     # Read the run config file
     params = read_run_config_file(args.run_config_file)
 
-    # Argument checking
+    # Verify the run config file's parameters
     params  = checkArgs(params)
     dl_only = params['download_only'] or args.download_only
 
