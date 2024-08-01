0bf14ab8887f0b91f278b48f8f8a4767b1d949db
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-08-01 16:34:06 -0500

    Fix typo
    I meant to turn the path into a string

--- tools/RAiDER/models/credentials.py
+++ tools/RAiDER/models/credentials.py
@@ -151,7 +151,7 @@ def check_api(model: str,
         # one that belongs to this URL.
         import netrc
         rc_path.touch()
-        netrc_credentials = netrc.netrc(rc_path.name)
+        netrc_credentials = netrc.netrc(str(rc_path))
         netrc_credentials.hosts[url] = (uid, '', key)
         rc_path.write_text(str(netrc_credentials))
     rc_path.chmod(0o000600)
