37684c72e0327e3b3d42a26f0bb4b058f557e63a
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-08-05 14:40:15 -0500

    Adapt for tmp_path

--- test/test_HRRR_ztd.py
+++ test/test_HRRR_ztd.py
@@ -1,17 +1,19 @@
-from test import TEST_DIR
+from test import TEST_DIR, pushd
 
 import numpy as np
 import xarray as xr
 from RAiDER.cli.raider import calcDelays
 
-def test_scenario_1(data_for_hrrr_ztd, mocker):
+def test_scenario_1(tmp_path, data_for_hrrr_ztd, mocker):
     SCENARIO_DIR = TEST_DIR / "scenario_1"
     test_path = SCENARIO_DIR / 'raider_example_1.yaml'
     mocker.patch('RAiDER.processWM.prepareWeatherModel',
                     side_effect=[str(data_for_hrrr_ztd)])
-    calcDelays([str(test_path)])
+    
+    with pushd(tmp_path):
+        calcDelays([str(test_path)])
+        new_data  = xr.load_dataset('HRRR_tropo_20200101T120000_ztd.nc')
 
-    new_data  = xr.load_dataset(SCENARIO_DIR / 'HRRR_tropo_20200101T120000_ztd.nc')
     new_data1 = new_data.sel(x=-91.84, y=36.84, z=0, method='nearest')
     golden_data = 2.2622863, 0.0361021 # hydro|wet
 
